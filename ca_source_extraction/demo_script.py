8e68eb47b86a324bc7dbd2027ac8516b26eacfe9
epnev <eap2111@columbia.edu>
2015-09-09 19:50:34 -0400

    clean up

--- ca_source_extraction/demo_script.py
+++ ca_source_extraction/demo_script.py
@@ -8,7 +8,11 @@ Created on Wed Sep  9 18:47:13 2015
 import pims
 import numpy as np
 import scipy.io as sio
+from arpfit import arpfit
 from greedyROI2d import greedyROI2d
+from sklearn.decomposition import ProjectedGradientNMF
+from update_spatial_components import update_spatial_components
+from update_temporal_components import update_temporal_components
 
 #%%
 #frm=pims.open('demoMovie.tif')
@@ -28,7 +32,6 @@ Ain,Cin,center = greedyROI2d(Y, nr = nr, gSig = [4,4], gSiz = [9,9])
 
 #%% arpfit
 
-from arpfit import arpfit
 active_pixels = np.squeeze(np.nonzero(np.sum(Ain,axis=1)))
 Yr = np.reshape(Y,(d1*d2,T),order='F')
 p = 2;
@@ -38,7 +41,6 @@ P = arpfit(Yr,p=2,pixels = active_pixels)
 
 Y_res = Yr - np.dot(Ain,Cin)
 
-from sklearn.decomposition import ProjectedGradientNMF
 model = ProjectedGradientNMF(n_components=1, init='random',
                              random_state=0)
 model.fit(np.maximum(Y_res,0)) 
@@ -46,12 +48,10 @@ model.fit(np.maximum(Y_res,0))
 fin = model.components_.squeeze()
 
 #%% update spatial components
-from update_spatial_components import update_spatial_components
 
 A,b = update_spatial_components(Yr, Cin, fin, Ain, d1=d1, d2=d2, sn = P['sn'])
 
 #%% update temporal components
 
-from update_temporal_components import update_temporal_components
 
 C,f,Y_res,Pnew = update_temporal_components(Yr,A,b,Cin,fin,ITER=2)
