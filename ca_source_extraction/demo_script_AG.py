e40eff1db7444e99696c1b14d2ba9bcc7482c76b
epnev <eap2111@columbia.edu>
2015-09-12 08:41:14 -0400

    demo cleanup

--- ca_source_extraction/demo_script_AG.py
+++ ca_source_extraction/demo_script_AG.py
@@ -66,10 +66,10 @@ t1 = time()
 C,f,Y_res,Pnew = update_temporal_components(Yr,A,b,Cin,fin,ITER=2)
 t_elTEMPORAL1 = time() - t1
 
-#%%  solving using spgl1 for deconvolution
-t1 = time()
-C2,f2,Y_res2,Pnew2 = update_temporal_components(Yr,A,b,Cin,fin,ITER=2,deconv_method = 'spgl1')
-t_elTEMPORAL2 = time() - t1
+#%%  solving using spgl1 for deconvolution (necessary files not in repo yet)
+#t1 = time()
+#C2,f2,Y_res2,Pnew2 = update_temporal_components(Yr,A,b,Cin,fin,ITER=2,deconv_method = 'spgl1')
+#t_elTEMPORAL2 = time() - t1
 #%% %%%%%%%%%%%%% ANDREA NEEDS TO FIX THIS %%%%%%%%%%%%%%%%%%%
 #SAVE TO FILE
 np.savez('preprocess_analysis',Y_res=Y_res,A=A.todense(),b=b,C=C,f=f,d1=d1,d2=d2,P=P,Pnew=Pnew,sn=P['sn'])
@@ -90,4 +90,4 @@ Pnew=vars_['Pnew']
 sn=vars_['sn']
 #%%
 from merge_rois import mergeROIS
-A_m,C_m,nr_m,merged_ROIs,P_m=mergeROIS(Y_res,A.tocsc(),b,C2,f2,d1,d2,Pnew2,sn=P['sn'])
+A_m,C_m,nr_m,merged_ROIs,P_m=mergeROIS(Y_res,A.tocsc(),b,C,f,d1,d2,Pnew,sn=P['sn'])
