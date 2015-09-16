18965c3f5473287eb6f0a06a3cc32a0e7967ee2e
epnev <eap2111@columbia.edu>
2015-09-16 09:32:21 -0400

    demo cleanup

--- ca_source_extraction/demo_script_AG.py
+++ ca_source_extraction/demo_script_AG.py
@@ -5,7 +5,6 @@ Created on Wed Sep  9 18:47:13 2015
 @author: epnevmatikakis
 """
 #%%
-import pims
 import numpy as np
 import scipy.io as sio
 from greedyROI2d import greedyROI2d
@@ -14,9 +13,9 @@ from sklearn.decomposition import ProjectedGradientNMF
 from update_spatial_components import update_spatial_components
 from update_temporal_components import update_temporal_components
 from matplotlib import pyplot as plt
+from time import time 
 from merge_rois import mergeROIS
 from scipy.sparse import coo_matrix
-#import libtiff
 import tifffile
 from utilities import *
 
@@ -27,18 +26,9 @@ from utilities import *
 t = tifffile.TiffFile('demoMovie.tif') 
 #t = libtiff.TiffFile('/Users/eftychios/Documents/_code/calcium_paper_code/datasets/clay/2014-04-05-003.tif')
 Y = t.asarray() 
-#Y2 = tt[:]*1.
 Y = np.transpose(Y,(1,2,0))*1.
+d1,d2,T=Y.shape
 
-#%%
-sizeY = np.shape(Y)
-d1 = sizeY[0]
-d2 = sizeY[1]
-T = sizeY[-1]
-##%%
-#Y = np.array(pims.open('demoMovie.tif')) 
-#Y=np.transpose(Y,(1,2,0))
-#d1,d2,T=Y.shape
 #%% greedy initialization
 nr = 30
 t1 = time()
@@ -48,6 +38,7 @@ t_elGREEDY = time()-t1
 #%% plot centers
 Cn = local_correlations(Y)
 fig = plt.figure()
+plt1 = plt.imshow(Cn,interpolation=None)
 plt.colorbar()
 
 plt.scatter(x=center[:,1], y=center[:,0], c='m', s=40)
@@ -106,26 +97,3 @@ view_patches(Yr,coo_matrix(A_or),C_or,b,f,d1,d2)
 crd = plot_contours(coo_matrix(A_or),Cn,thr=0.9)
 #SAVE TO FILE
 #np.savez('preprocess_analysis',Y_res=Y_res,A=A.todense(),b=b,C=C,f=f,d1=d1,d2=d2,P=P,Pnew=Pnew,sn=P['sn'])
-
-#%% %%%%%%%%%%%%% ANDREA NEEDS TO FIX THIS %%%%%%%%%%%%%%%%%%%
-##SAVE TO FILE
-#np.savez('preprocess_analysis',Y_res=Y_res,A=A.todense(),b=b,C=C,f=f,d1=d1,d2=d2,P=P,Pnew=Pnew,sn=P['sn'])
-#
-#import numpy as np
-#from scipy.sparse import csc_matrix,coo_matrix
-#vars_=np.load('preprocess_analysis.npz')
-#
-#Y_res=vars_['Y_res']
-#A=coo_matrix(vars_['A'])
-#b=vars_['b']
-#C=vars_['C']
-#f=vars_['f']
-#d1=vars_['d1']
-#d2=vars_['d2']
-#P=vars_['P']
-#Pnew=vars_['Pnew']
-#sn=vars_['sn']
-##%%
-#from merge_rois import mergeROIS
-#A_m,C_m,nr_m,merged_ROIs,P_m=mergeROIS(Y_res,A.tocsc(),b,C,f,d1,d2,Pnew,sn=sn)
-#A_m,C_m,nr_m,merged_ROIs,P_m=mergeROIS(Y_res,A.tocsc(),b,C,f,d1,d2,Pnew,sn=P['sn'])
