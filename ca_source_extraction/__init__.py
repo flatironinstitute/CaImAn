1bc794c383bd90687d1cf28f863c6bec97675047
Andrea Giovannucci <agiovann@AndreaGiovannucci2015RetinaMacbookPro.local>
2015-12-27 21:14:09 -0500

    removed arpfit from init file

--- ca_source_extraction/__init__.py
+++ ca_source_extraction/__init__.py
@@ -1,4 +1,4 @@
-from .initialization import initialize_components,arpfit
+from .initialization import initialize_components
 from .spatial import update_spatial_components
 from .temporal import update_temporal_components
 from .merging import mergeROIS
