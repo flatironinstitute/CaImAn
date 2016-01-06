4e5e5769179b9d029d8e8ca8dfbe47b35c0a3d98
Andrea Giovannucci <agiovann@AndreaGiovannucci2015RetinaMacbookPro.local>
2016-01-06 17:22:30 -0500

    added large data demo

--- examples/demo_large_data.py
+++ examples/demo_large_data.py
@@ -24,7 +24,8 @@ import tifffile
 import subprocess
 import time as tm
 from time import time
-#%% for caching
+import pandas as pd
+#% for caching
 caching=1
 
 if caching:
@@ -32,21 +33,30 @@ if caching:
     import shutil
     import os
 
-#%% TYPE DECONVOLUTION
+#% TYPE DECONVOLUTION
 #deconv_type='debug'
 deconv_type='spgl1'
 large_data=False
+remove_baseline=False
+window_baseline=700
+quantile_baseline=.1
 #%%
-
+reload=0
 filename='movies/demoMovie.tif'
-if 1:
+if not reload:
     t = tifffile.TiffFile(filename) 
-    Y = t.asarray() 
-    Y = np.transpose(Y,(1,2,0))*1.
+    Y = t.asarray().astype(dtype=np.float32) 
+    Y = np.transpose(Y,(1,2,0))
     d1,d2,T=Y.shape
+    Yr=np.reshape(Y,(d1*d2,T),order='F')
+    if remove_baseline:        
+        Yr_begin=Yr[:,:99].copy()
+        Yr=Yr-pd.rolling_quantile(Yr.T,window_baseline,quantile_baseline,min_periods=100,center=True).T
+        Yr[:,:99]=Yr_begin-np.percentile(Yr_begin,quantile_baseline*100,axis=1)[:,None]    
+        Y=np.reshape(Yr,(d1,d2,T),order='F')
     np.save('Y',Y)
-    np.save('Yr',np.reshape(Y,(d1*d2,T),order='F'))
-
+    np.save('Yr',Yr)
+#%    
 if caching:
     Y=np.load('Y.npy',mmap_mode='r')
     Yr=np.load('Yr.npy',mmap_mode='r')        
@@ -61,7 +71,7 @@ if not large_data:
 else:
     Cn=np.mean(Y,axis=2) 
 #%%
-n_processes=4
+n_processes=8
 n_pixels_per_process=d1*d2/n_processes
 p=2;
 
@@ -72,7 +82,8 @@ preprocess_params={ 'sn':None, 'g': None, 'noise_range' : [0.25,0.5], 'noise_met
 init_params = { 
                     'K':30,'gSig':[4,4],'gSiz':[9,9], 
                     'ssub':1,'tsub':1,
-                    'nIter':5, 'use_median':False, 'kernel':None
+                    'nIter':5, 'use_median':False, 'kernel':None,
+                    'maxIter':5
                     }
 #%% PREPROCESS DATA
 t1 = time()
@@ -106,40 +117,20 @@ plt.scatter(x=center[:,1], y=center[:,0], c='m', s=40)
 crd = cse.plot_contours(coo_matrix(Ain[:,::-1]),Cn,thr=0.9)
 plt.axis((-0.5,d2-0.5,-0.5,d1-0.5))
 plt.gca().invert_yaxis()
-
+pl.show()
 #%% start cluster for efficient computation
 print "Starting Cluster...."
 sys.stdout.flush()    
 p=subprocess.Popen(["ipcluster start -n " + str(n_processes)],shell=True) 
-tm.sleep(10)    
+tm.sleep(6)    
 #%%
 t1 = time()
 A,b,Cin = cse.update_spatial_components_parallel(Yr, Cin, f_in, Ain, sn=sn, **spatial_params)
 t_elSPATIAL = time() - t1
 print t_elSPATIAL 
-#%%
-#if 1:
-#    np.savez('tmp_temporal.npz',Yr=Yr,A=A.todense(),b=b,Cin=Cin,f_in=f_in,temporal_params=temporal_params)
-#else:
-#    with np.load('tmp_temporal.npz') as fl:
-#        Yr=fl['Yr']
-#        A=fl['A']
-#        b=fl['b']
-#        Cin=fl['Cin']
-#        f_in=fl['f_in']
-#        temporal_params=fl['temporal_params']
-       
     
 #%%
 crd = cse.plot_contours(A,Cn,thr=0.9)
-#%%
-#t1 = time()
-#Axxx,bxxx,Cinxxx = cse.update_spatial_components(Yr, Cin, f_in, Ain, d1=d1, d2=d2, sn = sn,dist=2,max_size=8,min_size=3)
-#t_elSPATIAL = time() - t1
-#print t_elSPATIAL#        
-#
-#pl.figure()
-#crd = cse.plot_contours(Axxx,np.mean(np.reshape(Yr,(d1,d2,np.shape(Yr)[-1]),order='F'),axis=2),thr=0.95)
 #%% restart cluster
 if caching:
     print "Stopping  and restarting cluster to avoid unnencessary use of memory...."
@@ -154,12 +145,48 @@ if caching:
 t1 = time()
 C,f,Y_res,S,bl,c1,neurons_sn,g = cse.update_temporal_components_parallel(Yr,A,b,Cin,f_in,bl=None,c1=None,sn=None,g=None,**temporal_params)
 t_elTEMPORAL2 = time() - t1
-print t_elTEMPORAL2 # took 98 sec    
+print t_elTEMPORAL2 # took 98 sec   
 #%%
-#t1 = time()
-#Cxx,fxx,Y_resxx,Pnewxx,Sxx = cse.update_temporal_components(Yr,A,b,Cin,f_in,**temporal_params)
-#t_elTEMPORAL2 = time() - t1
-#print t_elTEMPORAL2 # took 98 sec 
+if 0: 
+    A_or, C_or, srt = cse.order_components(A,C)
+    cse.view_patches(Yr,coo_matrix(A_or),C_or,b,f,d1,d2,secs=1)
+else:
+    cse.view_patches(Yr,A,C,b,f,d1,d2,secs=1)
+    C_or=C
+    A_or=A.todense()
+    srt=range(30)
+#%%
+np.savez('temp_spatial.npz',A_or=A_or, C_or=C_or, srt=srt,C=C,f=f,Y_res=Y_res,S=S,bl=bl,c1=c1,neurons_sn=neurons_sn,g=g,Yr=Yr,A=A,b=b,Cin=Cin,f_in=f_in,temporal_params=temporal_params)
+#%%
+idx_= 16   #8 12
+for idx_ in [16]:#range(30):
+    nA2 = np.sum(np.array(A_or)**2,axis=0)
+    Ctmp=C_or.copy()
+    Atmp=A_or.copy()        
+    Atmp[:,idx_]=0  
+    Ctmp[idx_,:]=0
+    tr=(Yr-b.dot(f)- Atmp.dot(Ctmp)).T.dot(A_or[:,idx_]).T/nA2[idx_]    
+    tr2=(Yr-b.dot(f)- Atmp.dot(Ctmp)).T.dot(A_or[:,idx_]).T
+#    temporal_params['method']='spgl1'
+    temporal_params['method']='debug'
+   
+    tr=np.squeeze(np.asarray(tr))
+    tr2=np.squeeze(np.asarray(tr2))
+    
+    cc_,cb_,c1_,gn_,sn_,sp_ = cse.deconvolution.constrained_foopsi(tr, bl = None,  c1 = None, g = None,  sn = None, **temporal_params)  
+    cc_2,cb_2,c1_2,gn_2,sn_2,sp_2 = cse.deconvolution.constrained_foopsi(tr2, bl = None,  c1 = None, g = None,  sn = None, **temporal_params)
+
+    #cc_,cb_,c1_,gn_,sn_,sp_ = cse.deconvolution.constrained_foopsi(tr, bl = cb_,  c1 = c1_, g = gn_,  sn = sn_, **temporal_params)
+    
+    pl.plot(cc_.T+cb_)
+    pl.plot(cc_2.T+cb_2)
+    pl.plot(tr.T)
+#    pl.plot(sp_.T)
+    pl.plot(C[srt[idx_],:].T)
+#    pl.plot(Cin[srt[idx_]])
+    plt.show()
+#    plt.waitforbuttonpress()
+#    plt.cla()
 #%%
 #if 1:
 #    np.savez('tmp_temporal_2.npz',Yr=Yr,Y_res=Y_res,A=A.todense(),b=b,C=C,f=f,S=S,bl=bl,c1=c1,sn=sn,g=g,temporal_params=temporal_params,spatial_params=spatial_params)
@@ -183,10 +210,55 @@ t1 = time()
 A_m,C_m,nr_m,merged_ROIs,S_m,bl_m,c1_m,sn_m,g_m=cse.mergeROIS_parallel(Y_res,A,b,C,f,S,sn,temporal_params, spatial_params, bl=bl, c1=c1, sn=neurons_sn, g=g, thr=0.8, mx=50)
 t_elMERGE = time() - t1
 print t_elMERGE  
+#%%
+if 1:
+    A_or, C_or, srt = cse.order_components(A_m,C_m)
+    cse.view_patches(Yr,coo_matrix(A_or),C_or,b,f, d1,d2,secs=1)
+#%%
+
+#%%
+from scipy.io import loadmat
+ld = loadmat('./after_merge.mat') 
+Ain_mat=ld['Ain']
+Cin_mat=ld['Cin']
+Yr_mat=ld['Yr']
+Y_mat=ld['Y']
+f_in_mat=ld['fin']
+sn_mat=ld['sn']
+A_mat=ld['A']
+b_mat=ld['b']
+C_mat=ld['C']
+f_mat=ld['f']
+Y_res_mat=ld['Y_res']
+P_mat=ld['P']
+S_mat=ld['S']
+
+neurons_sn_mat=ld['neurons_sn'].flatten()
+bl_mat=ld['bl'].flatten()
+c1_mat=ld['c1'].flatten()
+g_mat=ld['g']
+g_mat=[[p1,p2] for p1,p2 in zip(g_mat[0],g_mat[1])]
+
+A_m_mat=ld['Am']
+K_m_mat=ld['K_m']
+C_m_mat=ld['Cm']
+merged_ROIs_mat=ld['merged_ROIs']
+P_m_mat=ld['P']
+S_m_mat=ld['Sm']
+#%%
+crd = cse.plot_contours(A_m,Cn,thr=0.9)
+#%%
+t1 = time()
+A2,b2,C2 = cse.update_spatial_components_parallel(Yr, C_m, f, A_m, sn=sn, **spatial_params)
+#C2,f2,Y_res2,S2,bl2,c12,neurons_sn2,g21 = cse.update_temporal_components_parallel(Yr,A2,b2,C2,f,bl=bl_m,c1=c1_m,sn=sn_m,g=g_m,**temporal_params)
+C2,f2,Y_res2,S2,bl2,c12,neurons_sn2,g21 = cse.update_temporal_components_parallel(Yr,A2,b2,C2,f,bl=None,c1=None,sn=None,g=None,**temporal_params)
+print time() - t1 # 100 seconds
+#%%
+if 1:
+    A_or, C_or, srt = cse.order_components(A2,C2)
+    cse.view_patches(Yr,coo_matrix(A_or),C_or,b2,f2, d1,d2,secs=1)
+    
 #%% STOP CLUSTER
 print "Stopping Cluster...."
 sys.stdout.flush()  
 q=subprocess.Popen(["ipcluster stop"],shell=True)
-tm.sleep(5)
-p.terminate()
-q.terminate()
