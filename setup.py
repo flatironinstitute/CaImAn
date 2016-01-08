8d2e6fcf6ce2e8375a3e5470d4a500acbe89ace1
Andrea Giovannucci <andrea.giovannucci@gmail.com>
2016-01-08 10:38:01 -0500

    added tools for automatic installation

---
+++ setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages,Extension
+from distutils.sysconfig import get_python_inc
+from os import path
+import os
+import numpy as np
+
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+here = path.abspath(path.dirname(__file__))
+    
+
+with open('README.md','r') as rmf:
+    readme = rmf.read()
+
+incdir = os.path.join(get_python_inc(plat_specific=1), 'Numerical')
+
+setup(	
+    name = 'Constrained_NMF',
+    version = '0.03',
+    author = 'Andrea Giovannucci and Eftychios Pnevmatikakis',
+    author_email = 'agiovannucci@simonsfoundation.org',
+    url = 'https://github.com/agiovann/Constrained_NMF',
+    license = 'GPL-2',
+    description = 'Advanced algorithms for ROI detection and deconvolution of Calcium Imaging datasets.',
+    long_description = readme,
+    		# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+    
+    # Indicate who your project is intended for
+    'Intended Audience :: Testers',
+    'Topic :: Calcium Imaging :: Analysis Tools',
+    
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: GPL-2 License',
+    
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2.7',
+    ],
+    keywords = 'fluorescence calcium ca imaging deconvolution ROI identification',
+    packages = ['ca_source_extraction'],
+    data_files = [	('', ['LICENSE.txt']),
+                  ('', ['README.md']),
+    												],
+    install_requires = [ 'matplotlib', 'scikit-learn', 'scikit-image', 'tifffile','ipyparallel','bokeh','jupyter','scikit-learn','joblib','cvxopt','ipython','picos', 'scipy','numpy'], 
+    include_dirs = [incdir, np.get_include()]    
+ 
+)
+
