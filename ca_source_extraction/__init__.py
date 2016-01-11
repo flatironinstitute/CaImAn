fd16bd7ad89102b1e352c3f508c787817188fc80
agiovann <andrea.giovannucci@gmail.com>
2016-01-10 20:33:32 -0500

    updated documentation

--- ca_source_extraction/__init__.py
+++ ca_source_extraction/__init__.py
@@ -3,7 +3,7 @@ from .spatial import update_spatial_components,update_spatial_components_paralle
 from .temporal import update_temporal_components,update_temporal_components_parallel
 from .merging import mergeROIS,mergeROIS_parallel
 from .pre_processing import preprocess_data
-from .utilities import local_correlations,plot_contours,view_patches,order_components,extract_DF_F,db_plot,nb_imshow, nb_view_patches,nb_plot_contour
+#from .utilities import com,local_correlations,plot_contours,view_patches,order_components,extract_DF_F,db_plot,nb_imshow, nb_view_patches,nb_plot_contour
 #import initialization, spatial, temporal, merging, utilities, pre_processing
 #from initialization import greedyROI2d,hals_2D
 #from spatial import update_spatial_components
