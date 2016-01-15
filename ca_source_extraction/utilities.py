8cd1f8aa00eae349a06e4aa6b9eaffd26d435e58
epnev <eap2111@columbia.edu>
2016-01-15 17:56:22 -0500

    modified notebook

--- ca_source_extraction/utilities.py
+++ ca_source_extraction/utilities.py
@@ -429,7 +429,10 @@ def nb_view_patches(Yr,A,C,b,f,d1,d2,image_neurons=None,thr = 0.99):
     grayp = [mpl.colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))]
     nr,T = C.shape
     nA2 = np.sum(np.array(A)**2,axis=0)
-    Y_r = np.array(spdiags(1/nA2,0,nr,nr)*(A.T*np.matrix(Yr-b[:,np.newaxis]*f[np.newaxis] - A.dot(C))) + C)  
+    b = np.squeeze(b)
+    f = np.squeeze(f)
+    #Y_r = np.array(spdiags(1/nA2,0,nr,nr)*(A.T*np.matrix(Yr-b[:,np.newaxis]*f[np.newaxis] - A.dot(C))) + C)    
+    Y_r = np.array(spdiags(1/nA2,0,nr,nr)*(A.T*np.matrix(Yr)-(A.T*np.matrix(b[:,np.newaxis]))*np.matrix(f[np.newaxis]) - (A.T.dot(A))*np.matrix(C)) + C)  
 
     
     bpl.output_notebook()
