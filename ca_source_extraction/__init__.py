8ba270c69ac05477bd7b22b8a0e86bbfa1d62781
epnev <eap2111@columbia.edu>
2016-01-15 17:44:41 -0500

    minor change

--- ca_source_extraction/__init__.py
+++ ca_source_extraction/__init__.py
@@ -4,7 +4,7 @@ from .temporal import update_temporal_components,update_temporal_components_para
 from .merging import mergeROIS,mergeROIS_parallel
 from .pre_processing import preprocess_data
 from .utilities import com,local_correlations,plot_contours,view_patches,order_components,extract_DF_F,db_plot,nb_imshow, nb_view_patches,nb_plot_contour
-#import initialization, spatial, temporal, merging, utilities, pre_processing
+import initialization, spatial, temporal, merging, utilities, pre_processing
 #from initialization import greedyROI2d,hals_2D
 #from spatial import update_spatial_components
 #from temporal import update_temporal_components
