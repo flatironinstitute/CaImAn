2311a7bd8064ce7b8e663cf3758c6060b5c5738f
valiseverywhere <val_is_everywhere@yahoo.com>
2016-04-24 18:55:30 -0700

    reverting to light version of setup.py from upstream

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from os import path
 
 """
     Installation script for anaconda installers
-
+    
 """
 here = path.abspath(path.dirname(__file__))
-
+    
 with open('README.md','r') as rmf:
     readme = rmf.read()
 
 #incdir = os.path.join(get_python_inc(plat_specific=1), 'Numerical')
 
-setup(
+setup(	
     name = 'Constrained_NMF',
     version = '0.1',
     author = 'Andrea Giovannucci and Eftychios Pnevmatikakis',
@@ -31,14 +31,14 @@ setup(
     #   4 - Beta
     #   5 - Production/Stable
     'Development Status :: 3 - Alpha',
-
+    
     # Indicate who your project is intended for
     'Intended Audience :: Testers',
     'Topic :: Calcium Imaging :: Analysis Tools',
-
+    
     # Pick your license as you wish (should match "license" above)
     'License :: OSI Approved :: GPL-2 License',
-
+    
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 2.7',
@@ -47,8 +47,8 @@ setup(
     packages = ['ca_source_extraction','SPGL1_python_port'],
     data_files = [	('', ['LICENSE.txt']),
                   ('', ['README.md'])],
-    install_requires = [ 'python==2.7.*','matplotlib', 'scikit-learn', 'scikit-image', 'ipyparallel','ipython','scipy','numpy','tifffile','cvxopt','picos','cvxpy','joblib>=0.8.4']
-    #include_dirs = [incdir, np.get_include()]
-    #,'bokeh','jupyter',
-
+    install_requires = [ 'python==2.7.*'],#'matplotlib', 'scikit-learn', 'scikit-image', 'ipyparallel','scikit-learn','ipython','scipy','numpy'],#,'bokeh','jupyter','tifffile','cvxopt','picos', 'joblib>=0.8.4'],
+    #include_dirs = [incdir, np.get_include()]    
+ 
 )
+
