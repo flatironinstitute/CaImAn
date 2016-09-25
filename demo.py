2b67d859968c6488f29d1f733e73b5a9ad8ea999
agiovann <andrea.giovannucci@gmail.com>
2016-09-25 16:42:11 -0400

    refactored demo

--- demo.py
+++ demo.py
@@ -1,38 +1,37 @@
 #%%
 try:
-#    %load_ext autoreload
-#    %autoreload 2
-    print 1
-except:
-    print 'NOT IPYTHON'
-
-import matplotlib as mpl
-mpl.use('TKAgg')
-from matplotlib import pyplot as plt
-#plt.ion()
+    if __IPYTHON__:
+        # this is used for debugging purposes only. allows to reload classes when changed
+        get_ipython().magic('%load_ext autoreload')
+        get_ipython().magic('%autoreload 2')
+except NameError:       
+    print('Not IPYTHON')    
+    pass
 
 import sys
 import numpy as np
-import ca_source_extraction as cse
-
-#sys.path.append('../SPGL1_python_port')
-#%
 from time import time
 from scipy.sparse import coo_matrix
-import tifffile
-import subprocess
-import time as tm
-from time import time
-import pylab as pl
 import psutil
 import glob
 import os
 import scipy
 from ipyparallel import Client
+import matplotlib as mpl
+#mpl.use('Qt5Agg')
+
+
+import pylab as pl
+pl.ion()
+#%%
+import ca_source_extraction as cse
+#
+#%%
+# frame rate in Hz
+final_frate=10 
 #%%
 n_processes = np.maximum(np.int(psutil.cpu_count()*.75),1) # roughly number of cores on your machine minus 1
 print 'using ' + str(n_processes) + ' processes'
-
 #%% start cluster for efficient computation
 single_thread=False
 
@@ -61,6 +60,7 @@ fnames=fnames
 #%% Create a unique file fot the whole dataset
 # THIS IS  ONLY IF YOU NEED TO SELECT A SUBSET OF THE FIELD OF VIEW 
 #fraction_downsample=1;
+#final_frate=final_frate*fraction_downsample
 #idx_x=slice(10,502,None)
 #idx_y=slice(10,502,None)
 #fname_new=cse.utilities.save_memmap(fnames,base_name='Yr',resize_fact=(1,1,fraction_downsample),remove_init=0,idx_xy=(idx_x,idx_y))
@@ -70,38 +70,31 @@ name_new=cse.utilities.save_memmap_each(fnames, dview=dview,base_name='Yr', resi
 name_new.sort()
 #%%
 fname_new=cse.utilities.save_memmap_join(name_new,base_name='Yr', n_chunks=12, dview=dview)
-#%%  Create a unique file fot the whole dataset
-#
-#fraction_downsample=1; # useful to downsample the movie across time. fraction_downsample=.1 measn downsampling by a factor of 10
+#%%  Create a unique file for the whole dataset
+# useful to downsample the movie across time. fraction_downsample=.1 measn downsampling by a factor of 10
+#fraction_downsample=1;
+#final_frate=final_frate*fraction_downsample
 #fname_new=cse.utilities.save_memmap(fnames,base_name='Yr',resize_fact=(1,1,fraction_downsample),order='C')
 #%%
-
-#%%
-#fname_new='Yr_d1_501_d2_398_d3_1_order_F_frames_369_.mmap'
 Yr,dims,T=cse.utilities.load_memmap(fname_new)
 Y=np.reshape(Yr,dims+(T,),order='F')
-#%%
+#%% visualize correlation image
 Cn = cse.utilities.local_correlations(Y[:,:,:])
 pl.imshow(Cn,cmap='gray')    
-
-#%%
+#%% parameters of experiment
 K=30 # number of neurons expected per patch
 gSig=[7,7] # expected half size of neurons
 merge_thresh=0.8 # merging threshold, max correlation allowed
 p=2 #order of the autoregressive system
 options = cse.utilities.CNMFSetParms(Y,n_processes,p=p,gSig=gSig,K=K,ssub=2,tsub=2)
-
 #%% PREPROCESS DATA AND INITIALIZE COMPONENTS
 t1 = time()
 Yr,sn,g,psx = cse.pre_processing.preprocess_data(Yr,dview=dview,**options['preprocess_params'])
 print time() - t1
-
-
 #%%
 t1 = time()
 Atmp, Ctmp, b_in, f_in, center=cse.initialization.initialize_components(Y, normalize=True, **options['init_params'])                                                    
 print time() - t1
-
 #%% Refine manually component by clicking on neurons 
 refine_components=False
 if refine_components:
@@ -144,46 +137,50 @@ options['temporal_params']['p'] = p # set it back to original value to perform f
 C2,f2,S2,bl2,c12,neurons_sn2,g21,YrA = cse.temporal.update_temporal_components(Yr,A2,b2,C2,f,dview=dview, bl=None,c1=None,sn=None,g=None,**options['temporal_params'])
 print time() - t1
 #%%
+tB = np.minimum(-2,np.floor(-5./30*final_frate))
+tA = np.maximum(5,np.ceil(25./30*final_frate))
+Npeaks=10
 traces=C2+YrA
-idx_components, fitness, erfc,r_values,num_significant_samples = cse.utilities.evaluate_components(Y,traces,A2,N=5,robust_std=False)
+#        traces_a=traces-scipy.ndimage.percentile_filter(traces,8,size=[1,np.shape(traces)[-1]/5])
+#        traces_b=np.diff(traces,axis=1)
+fitness_raw, fitness_delta, erfc_raw, erfc_delta, r_values, significant_samples = cse.utilities.evaluate_components(Y, traces, A2, C2, b2, f2, remove_baseline=True, N=5, robust_std=False, Athresh = 0.1, Npeaks = Npeaks, tB=tB, tA = tA, thresh_C = 0.3)
 
-sure_in_idx= idx_components[np.logical_and(np.array(num_significant_samples)>1 ,np.array(r_values)>=.5)]
-doubtful = idx_components[np.logical_and(np.array(num_significant_samples)==1 ,np.array(r_values)>=.5)]
-they_suck = idx_components[np.logical_and(np.array(num_significant_samples)>=0 ,np.array(r_values)<.5)]
-#%%
-cse.utilities.view_patches_bar(Yr,scipy.sparse.coo_matrix(A2.tocsc()[:,sure_in_idx]),C2[sure_in_idx,:],b2,f2, dims[0],dims[1], YrA=YrA[sure_in_idx,:],img=Cn)  
+idx_components_r=np.where(r_values>=.6)[0]
+idx_components_raw=np.where(fitness_raw<-60)[0]        
+idx_components_delta=np.where(fitness_delta<-20)[0]   
+
+
+min_radius=gSig[0]-2
+masks_ws,idx_blobs,idx_non_blobs=cse.utilities.extract_binary_masks_blob(
+A2.tocsc(), min_radius, dims, num_std_threshold=1, 
+minCircularity= 0.6, minInertiaRatio = 0.2,minConvexity =.8)
+
+
+
+
+idx_components=np.union1d(idx_components_r,idx_components_raw)
+idx_components=np.union1d(idx_components,idx_components_delta)  
+idx_blobs=np.intersect1d(idx_components,idx_blobs)   
+idx_components_bad=np.setdiff1d(range(len(traces)),idx_components)
+
+print(' ***** ')
+print len(traces)
+print(len(idx_components))
+print(len(idx_blobs))
 #%% visualize components
 #pl.figure();
 pl.subplot(1,3,1)
-crd = cse.utilities.plot_contours(A2.tocsc()[:,sure_in_idx],Cn,thr=0.9)
+crd = cse.utilities.plot_contours(A2.tocsc()[:,idx_components],Cn,thr=0.9)
 pl.subplot(1,3,2)
-crd = cse.utilities.plot_contours(A2.tocsc()[:,doubtful],Cn,thr=0.9)
+crd = cse.utilities.plot_contours(A2.tocsc()[:,idx_blobs],Cn,thr=0.9)
 pl.subplot(1,3,3)
-crd = cse.utilities.plot_contours(A2.tocsc()[:,they_suck],Cn,thr=0.9)
+crd = cse.utilities.plot_contours(A2.tocsc()[:,idx_components_bad],Cn,thr=0.9)
+#%%
+cse.utilities.view_patches_bar(Yr,scipy.sparse.coo_matrix(A2.tocsc()[:,idx_components]),C2[idx_components,:],b2,f2, dims[0],dims[1], YrA=YrA[idx_components,:],img=Cn)  
+#%%
+cse.utilities.view_patches_bar(Yr,scipy.sparse.coo_matrix(A2.tocsc()[:,idx_components_bad]),C2[idx_components_bad,:],b2,f2, dims[0],dims[1], YrA=YrA[idx_components_bad,:],img=Cn)  
 #%% STOP CLUSTER
 pl.close()
 if not single_thread:    
     c.close()
     cse.utilities.stop_server()
-
-##%% select  blobs
-#min_radius=5 # min radius of expected blobs
-#masks_ws,pos_examples,neg_examples=cse.utilities.extract_binary_masks_blob(A2.tocsc()[:,:], 
-#     min_radius, dims, minCircularity= 0.5, minInertiaRatio = 0.2,minConvexity = .8)
-#
-#pl.subplot(1,2,1)
-#
-#final_masks=np.array(masks_ws)[pos_examples]
-#pl.imshow(np.reshape(final_masks.max(0),dims,order='F'),vmax=1)
-#pl.subplot(1,2,2)
-#
-#neg_examples_masks=np.array(masks_ws)[neg_examples]
-#pl.imshow(np.reshape(neg_examples_masks.max(0),dims,order='F'),vmax=1)
-##%%
-#pl.imshow(np.reshape(A2.tocsc()[:,neg_examples].mean(1),dims, order='F'))
-##%%
-#pl.imshow(np.reshape(A2.tocsc()[:,pos_examples].mean(1),dims, order='F'))
-##%%
-#cse.utilities.view_patches_bar(Yr,scipy.sparse.coo_matrix(A2.tocsc()[:,pos_examples]),C2[pos_examples,:],b2,f2, dims[0],dims[1], YrA=YrA[pos_examples,:],img=Cn)  
-##%%
-#cse.utilities.view_patches_bar(Yr,scipy.sparse.coo_matrix(A2.tocsc()[:,neg_examples]),C2[neg_examples,:],b2,f2, dims[0],dims[1], YrA=YrA[neg_examples,:],img=Cn)  
