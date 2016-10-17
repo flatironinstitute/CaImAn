8c0ba27ada2f80d309391c5e256e1fe36d600f10
Andrea Giovannucci <andrea.giovannucci@gmail.com>
2016-10-17 14:29:25 -0400

    removed calblits import lines

--- caiman/source_extraction/cnmf/__init__.py
+++ caiman/source_extraction/cnmf/__init__.py
@@ -1,8 +1,8 @@
 #%%
 import sys
 import os
-cse_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(cse_dir, 'CalBlitz'))
+#cse_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.join(cse_dir, 'CalBlitz'))
 import initialization, spatial, temporal, merging, utilities, pre_processing, map_reduce
 from initialization import initialize_components, greedyROI
 from spatial import update_spatial_components
