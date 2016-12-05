e21d2841e7fa474fb7252f6ce64a4c98bf97c170
agiovann <andrea.giovannucci@gmail.com>
2016-12-04 23:53:34 -0500

    removed content of dft registration

--- caiman/dft_registration.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 22 20:03:46 2016
-
-Port of Manuel Guizar's code from:
-http://www.mathworks.com/matlabcentral/fileexchange/18401-efficient-subpixel-image-registration-by-cross-correlation
-
