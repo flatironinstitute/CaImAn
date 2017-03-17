f4ded6f5fb1e2c5ffed5bfc1df429a4b3d6f45a5
ecobost <emcobost@gmail.com>
2017-03-16 22:00:12 -0500

    Changed the thresholding direction so everything that as default everything that is not zero is accepted and a single connected component per mask is created

--- caiman/source_extraction/cnmf/spatial.py
+++ caiman/source_extraction/cnmf/spatial.py
@@ -36,8 +36,7 @@ try:
 except:
     print('cvxopt not installed')
 
-import pylab as pl
-from caiman.mmapping import load_memmap, parallel_dot_product
+from ...mmapping import load_memmap, parallel_dot_product
 from scipy.ndimage.filters import median_filter
 from scipy.ndimage.morphology import binary_closing
 from scipy.ndimage.measurements import label
@@ -292,22 +291,6 @@ def update_spatial_components(Y, C=None, f=None, A_in=None, sn=None, dims=None, 
             for pars in chunk:
                 px, idxs_, a = pars
                 A_[px, idxs_] = a
-##
-#        Cf_ = [Cf[idx_, :] for idx_ in ind2_]
-#
-#        #% LARS regression
-#        A_ = np.hstack((np.zeros((d, nr)), np.zeros((d, np.size(f, 0)))))
-#
-#        for c, y, s, id2_, px in zip(Cf_, Y, sn, ind2_, range(d)):
-#            if px % 1000 == 0:
-#                print px
-#            if np.size(c) > 0:
-#                _, _, a, _, _ = lars_regression_noise_old(y, np.array(c.T), 1, sn[px]**2 * T)
-#                if np.isscalar(a):
-#                    A_[px, id2_] = a
-#                else:
-#                    A_[px, id2_] = a.T
-##
 
     #%
     print('Updated Spatial Components')
@@ -357,11 +340,6 @@ def update_spatial_components(Y, C=None, f=None, A_in=None, sn=None, dims=None, 
 
         raise Exception("Failed to delete: " + folder)
 
-
-    # if A_in.dtype == bool:
-    #     return A_, b, C, f
-    # else:
-    #     return A_, b, C
     return A_, b, C, f
 
 
@@ -638,36 +616,6 @@ def threshold_components(A, dims, medw=None, thr_method='nrg', maxthr=0.1, nrgth
         At, i = r
         Ath[:, i] = At
 
-#    for i in range(nr):
-#
-#        A_temp = np.reshape(A[:, i], dims[::-1])
-#        A_temp = median_filter(A_temp, medw)
-#        Asor = np.sort(np.squeeze(np.reshape(A_temp, (d, 1))))[::-1]
-#        temp = np.cumsum(Asor**2)
-#        ff = np.squeeze(np.where(temp < (1 - thr) * temp[-1]))
-#
-#        if ff.size > 0:
-#            if ff.ndim == 0:
-#                ind = ff
-#            else:
-#                ind = ff[-1]
-#            A_temp[A_temp < Asor[ind]] = 0
-#            BW = (A_temp >= Asor[ind])
-#        else:
-#            BW = (A_temp >= 0)
-#
-#        Ath[:, i] = np.squeeze(np.reshape(A_temp, (d, 1)))
-#        BW = binary_closing(BW.astype(np.int), structure=se)
-#        labeled_array, num_features = label(BW, structure=ss)
-#        BW = np.reshape(BW, (d, 1))
-#        labeled_array = np.squeeze(np.reshape(labeled_array, (d, 1)))
-#        nrg = np.zeros((num_features, 1))
-#        for j in range(num_features):
-#            nrg[j] = np.sum(Ath[labeled_array == j + 1, i]**2)
-#
-#        indm = np.argmax(nrg)
-#        Ath[labeled_array == indm + 1, i] = A[labeled_array == indm + 1, i]
-
     return Ath
 
 
@@ -681,7 +629,7 @@ def threshold_components_parallel(pars):
     elif thr_method == 'nrg':
         Asor = np.sort(np.squeeze(np.reshape(A_temp, (d, 1))))[::-1]
         temp = np.cumsum(Asor**2)
-        ff = np.squeeze(np.where(temp < (1 - nrgthr) * temp[-1]))
+        ff = np.squeeze(np.where(temp < nrgthr * temp[-1]))
         if ff.size > 0:
             if ff.ndim == 0:
                 ind = ff
@@ -690,11 +638,15 @@ def threshold_components_parallel(pars):
             A_temp[A_temp < Asor[ind]] = 0
             BW = (A_temp >= Asor[ind])
         else:
-            BW = (A_temp >= 0)
+            BW =np.zeros_like(A_temp)
 
     Ath = np.squeeze(np.reshape(A_temp, (d, 1)))
     Ath2 = np.zeros((d))
     BW = binary_closing(BW.astype(np.int), structure=se)
+
+    if BW.max() == 0:
+        return Ath2, i
+
     if extract_cc:
         labeled_array, num_features = label(BW, structure=ss)
         BW = np.reshape(BW, (d, 1))
@@ -706,6 +658,7 @@ def threshold_components_parallel(pars):
         indm = np.argmax(nrg)
         #Ath2[labeled_array == indm + 1] = A_i[labeled_array == indm + 1]
         Ath2[labeled_array == indm + 1] = Ath[labeled_array == indm + 1]
+
     else:
         BW = BW.flatten()
         Ath2[BW] = Ath[BW]
