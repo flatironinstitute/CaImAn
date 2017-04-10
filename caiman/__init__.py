103a25e8894f9212cb11faf46f9f763b9f980bf1
andrea.giovannucci@gmail.com <andrea.giovannucci@gmail.com>
2017-04-10 11:54:58 -0400

    adjusting some files

--- caiman/__init__.py
+++ caiman/__init__.py
@@ -3,4 +3,4 @@ from .base.timeseries import concatenate
 from .cluster import start_server,stop_server
 from .mmapping import load_memmap,save_memmap,save_memmap_each,save_memmap_join
 from .summary_images import local_correlations 
-from .source_extraction import cnmf
+#from .source_extraction import cnmf
