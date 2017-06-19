31065bea13385cb381b8d5c2cae9b14a49268217
ecobost <emcobost@gmail.com>
2017-06-19 17:39:25 -0500

    Delete double print of stopping cluster (l. 430)

--- caiman/cluster.py
+++ caiman/cluster.py
@@ -25,7 +25,7 @@ from .mmapping import load_memmap
 #%%
 def get_patches_from_image(img,shapes,overlaps):
     d1,d2 = np.shape(img)
-    rf =  np.divide(shapes,2)    
+    rf =  np.divide(shapes,2)
     _,coords_2d  = extract_patch_coordinates(d1,d2,rf=rf,stride = overlaps)
     imgs = np.empty(coords_2d.shape[:2],dtype = np.object)
 
@@ -39,11 +39,11 @@ def extract_patch_coordinates_old(d1,d2,rf=(7,7),stride = (2,2)):
     """
     Function that partition the FOV in patches and return the indexed in 2D and 1D (flatten, order='F') formats
     Parameters
-    ----------    
+    ----------
     d1,d2: int
         dimensions of the original matrix that will be  divided in patches
     rf: int
-        radius of receptive field, corresponds to half the size of the square patch        
+        radius of receptive field, corresponds to half the size of the square patch
     stride: int
         degree of overlap of the patches
     """
@@ -53,14 +53,14 @@ def extract_patch_coordinates_old(d1,d2,rf=(7,7),stride = (2,2)):
     stride1,stride2 = stride
     iter_0 = list(range(rf1,d1-rf1,2*rf1-stride1))+[d1-rf1]
     iter_1 = list(range(rf2,d2-rf2,2*rf2-stride2))+[d2-rf2]
-    coords_2d = np.empty([len(iter_0),len(iter_1),2],dtype=np.object)   
+    coords_2d = np.empty([len(iter_0),len(iter_1),2],dtype=np.object)
     for count_0,xx in enumerate(iter_0):
-        coords_x=np.array(list(range(xx - rf1, xx + rf1 + 1))) 
+        coords_x=np.array(list(range(xx - rf1, xx + rf1 + 1)))
         coords_x = coords_x[(coords_x >= 0) & (coords_x < d1)]
         for count_1,yy in enumerate(iter_1):
 
 
-            coords_y = np.array(list(range(yy - rf2, yy + rf2 + 1)))  
+            coords_y = np.array(list(range(yy - rf2, yy + rf2 + 1)))
 #            print([xx - rf1, xx + rf1 + 1,yy - rf2, yy + rf2 + 1])
             coords_y = coords_y[(coords_y >= 0) & (coords_y < d2)]
 
@@ -126,20 +126,20 @@ def extract_rois_patch(file_name,d1,d2,rf=5,stride = 5):
     n_components=2
     tol=1e-6
     max_iter=5000
-    args_in=[]    
-    for id_f,id_2d in zip(idx_flat[:],idx_2d[:]):        
+    args_in=[]
+    for id_f,id_2d in zip(idx_flat[:],idx_2d[:]):
         args_in.append((file_name, id_f,id_2d, perctl,n_components,tol,max_iter))
     st=time.time()
     print((len(idx_flat)))
     try:
         if 1:
-            c = Client()   
+            c = Client()
             dview=c[:]
-            file_res = dview.map_sync(nmf_patches, args_in)                         
+            file_res = dview.map_sync(nmf_patches, args_in)
         else:
-            file_res = list(map(nmf_patches, args_in))                         
+            file_res = list(map(nmf_patches, args_in))
     finally:
-        dview.results.clear()   
+        dview.results.clear()
         c.purge_results('all')
         c.purge_everything()
         c.close()
@@ -152,8 +152,8 @@ def extract_rois_patch(file_name,d1,d2,rf=5,stride = 5):
     C2=[]
     for count,f in enumerate(file_res):
         idx_,flt,ca,d=f
-        A1[idx_,count]=flt[:,0][:,np.newaxis]        
-        A2[idx_,count]=flt[:,1][:,np.newaxis]        
+        A1[idx_,count]=flt[:,0][:,np.newaxis]
+        A2[idx_,count]=flt[:,1][:,np.newaxis]
         C1.append(ca[0,:])
         C2.append(ca[1,:])
 #        pl.imshow(np.reshape(flt[:,0],d,order='F'),vmax=10)
@@ -166,16 +166,16 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
     '''
     apply function to patches in parallel or not
 
-    Parameters    
-    ----------        
+    Parameters
+    ----------
     file_name: string
-        full path to an npy file (2D, pixels x time) containing the movie        
+        full path to an npy file (2D, pixels x time) containing the movie
 
     shape: tuple of three elements
-        dimensions of the original movie across y, x, and time 
+        dimensions of the original movie across y, x, and time
 
 
-    rf: int 
+    rf: int
         half-size of the square patch in pixel
 
     stride: int
@@ -189,7 +189,7 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
     Returns
     -------
     results
-    '''    
+    '''
 
     (T,d1,d2)=shape
     d=d1*d2
@@ -200,7 +200,7 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
         rf1=rf
         rf2=rf
 
-    if not np.isscalar(stride):    
+    if not np.isscalar(stride):
         stride1,stride2=stride
     else:
         stride1=stride
@@ -214,13 +214,13 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
     if d1 <= rf1*2:
         shape_grid = (1,shape_grid[1])
     if d2 <= rf2*2:
-        shape_grid = (shape_grid[0],1)    
+        shape_grid = (shape_grid[0],1)
 
     print(shape_grid)
 
-    args_in=[]    
+    args_in=[]
 
-    for id_f,id_2d in zip(idx_flat[:],idx_2d[:]):        
+    for id_f,id_2d in zip(idx_flat[:],idx_2d[:]):
 
         args_in.append((mmap_file.filename, id_f,id_2d, function, args, kwargs))
 
@@ -233,12 +233,12 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
 
         try:
 
-            file_res = dview.map_sync(function_place_holder, args_in)  
+            file_res = dview.map_sync(function_place_holder, args_in)
 
-            dview.results.clear()   
+            dview.results.clear()
 
         except:
-            print('Something went wrong')  
+            print('Something went wrong')
             raise
         finally:
             print('You may think that it went well but reality is harsh')
@@ -246,18 +246,18 @@ def apply_to_patch(mmap_file, shape, dview, rf , stride , function, *args, **kwa
 
     else:
 
-        file_res = list(map(function_place_holder, args_in))      
+        file_res = list(map(function_place_holder, args_in))
 
     return file_res, idx_flat, shape_grid
 #%%
 def function_place_holder(args_in):
 
     file_name, idx_,shapes,function, args, kwargs = args_in
-    Yr, _, _ = load_memmap(file_name)   
+    Yr, _, _ = load_memmap(file_name)
     Yr = Yr[idx_,:]
     Yr.filename=file_name
-    d,T=Yr.shape      
-    Y=np.reshape(Yr,(shapes[1],shapes[0],T),order='F').transpose([2,0,1])           
+    d,T=Yr.shape
+    Y=np.reshape(Yr,(shapes[1],shapes[0],T),order='F').transpose([2,0,1])
     [T,d1,d2]=Y.shape
 
     res_fun = function(Y,*args,**kwargs)
@@ -280,7 +280,7 @@ def start_server(slurm_script=None, ipcluster="ipcluster", ncpus = None):
         number of processors
     ipcluster : str
         ipcluster binary file name; requires 4 path separators on Windows. ipcluster="C:\\\\Anaconda2\\\\Scripts\\\\ipcluster.exe"
-         Default: "ipcluster"    
+         Default: "ipcluster"
     '''
     sys.stdout.write("Starting cluster...")
     sys.stdout.flush()
@@ -354,7 +354,7 @@ def stop_server( ipcluster='ipcluster',pdir=None,profile=None):
     except:
         print('NOT SLURM')
         is_slurm = False
-        
+
     if is_slurm:
 
         if pdir is None and profile is None:
@@ -405,21 +405,21 @@ def stop_server( ipcluster='ipcluster',pdir=None,profile=None):
 #%%
 def setup_cluster(backend = 'local',n_processes = None,single_thread = False):
     ''' Restart if necessary the pipyparallel cluster, and manages the case of SLURM
-    
+
     '''
     #backend; 'local' or 'SLURM'. SLURM is experimental! You need to modify the script SLURM/slurmStart.sh
     if n_processes is None:
-        if backend == 'SLURM':            
+        if backend == 'SLURM':
             n_processes = np.int(os.environ.get('SLURM_NPROCS'))
         else:
             # roughly number of cores on your machine minus 1
-            
+
             n_processes = np.maximum(np.int(psutil.cpu_count()), 1)
-        
+
     print(('using ' + str(n_processes) + ' processes'))
-   
-    
-    
+
+
+
     if single_thread:
         dview = None
         c = None
@@ -427,12 +427,10 @@ def setup_cluster(backend = 'local',n_processes = None,single_thread = False):
         try:
             c.close()
         except:
-            print('C was not existing, creating one')
-            
-        print("Stopping  cluster to avoid unnencessary use of memory....")
-        
+            print('Client did not exist, creating one')
+
         sys.stdout.flush()
-        
+
         if backend == 'SLURM':
             try:
                 stop_server(is_slurm=True)
@@ -446,9 +444,8 @@ def setup_cluster(backend = 'local',n_processes = None,single_thread = False):
             stop_server()
             start_server(ncpus=n_processes)
             c = Client()
-    
+
         print(('Using ' + str(len(c)) + ' processes'))
         dview = c[:len(c)]
-    
-    return c,dview,n_processes                 
-    
+
+    return c,dview,n_processes
