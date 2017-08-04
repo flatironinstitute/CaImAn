7e8f56918647929c7c6f7f49d10daa944200ed24
andrea.giovannucci@gmail.com <andrea.giovannucci@gmail.com>
2017-08-04 09:28:25 -0400

    restored previous version of extrac reoi from imagej ZIPs

--- caiman/base/rois.py
+++ caiman/base/rois.py
@@ -422,19 +422,27 @@ def nf_masks_to_json(binary_masks,json_filename):
 
 
 def nf_read_roi(fileobj):
-    """
+    '''
     points = read_roi(fileobj)
-
     Read ImageJ's ROI format
     
     Addapted from https://gist.github.com/luispedro/3437255
-    Copyright: Luis Pedro Coelho <luis@luispedro.org>, 2012
-     License: MIT
-     This is based on:
-         http://rsbweb.nih.gov/ij/developer/source/ij/io/RoiDecoder.java.html
-         http://rsbweb.nih.gov/ij/developer/source/ij/io/RoiEncoder.java.html
-    """
+    '''
+# This is based on:
+# http://rsbweb.nih.gov/ij/developer/source/ij/io/RoiDecoder.java.html
+# http://rsbweb.nih.gov/ij/developer/source/ij/io/RoiEncoder.java.html
+
+
+    SPLINE_FIT = 1
+    DOUBLE_HEADED = 2
+    OUTLINE = 4
+    OVERLAY_LABELS = 8
+    OVERLAY_NAMES = 16
+    OVERLAY_BACKGROUNDS = 32
+    OVERLAY_BOLD = 64
     SUB_PIXEL_RESOLUTION = 128
+    DRAW_OFFSET = 256
+
 
     pos = [4]
     def get8():
@@ -461,17 +469,44 @@ def nf_read_roi(fileobj):
     magic = fileobj.read(4)
     if magic != 'Iout':
         raise IOError('Magic number not found')
+    version = get16()
 
     # It seems that the roi type field occupies 2 Bytes, but only one is used
+
+    roi_type = get8()
+    # Discard second Byte:
     get8()
+
+#    if not (0 <= roi_type < 11):
+#        print(('roireader: ROI type %s not supported' % roi_type))
+#
+#    if roi_type != 7:
+#
+#        print(('roireader: ROI type %s not supported (!= 7)' % roi_type))
+
     top = get16()
     left = get16()
-
+    bottom = get16()
+    right = get16()
     n_coordinates = get16()
+
+    x1 = getfloat() 
+    y1 = getfloat() 
+    x2 = getfloat() 
+    y2 = getfloat()
+    stroke_width = get16()
+    shape_roi_size = get32()
+    stroke_color = get32()
+    fill_color = get32()
     subtype = get16()
     if subtype != 0:
         raise ValueError('roireader: ROI subtype %s not supported (!= 0)' % subtype)
     options = get16()
+    arrow_style = get8()
+    arrow_head_size = get8()
+    rect_arc_size = get16()
+    position = get32()
+    header2offset = get32()
 
     if options & SUB_PIXEL_RESOLUTION:
         getc = getfloat
@@ -484,6 +519,7 @@ def nf_read_roi(fileobj):
     points[:,1] += left
     points[:,0] += top
     points -= 1
+
 
     return points
 
