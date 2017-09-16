e517648e2bf6296fc387106fd6efa5400864f5db
andrea.giovannucci@gmail.com <andrea.giovannucci@gmail.com>
2017-09-16 16:05:32 -0400

    now ground truth is build with rolling sum initialization

--- caiman/source_extraction/cnmf/cnmf.py
+++ caiman/source_extraction/cnmf/cnmf.py
@@ -65,7 +65,7 @@ class CNMF(object):
                  method_deconvolution = 'oasis', n_pixels_per_process = 4000, block_size = 20000,
                  check_nan = True, skip_refinement = False, normalize_init=True, options_local_NMF = None,
                  remove_very_bad_comps = False, border_pix = 0, low_rank_background = True, update_background_components = True,
-                 rolling_sum = False, rolling_length = 100):
+                 rolling_sum = True, rolling_length = 100):
         """ 
         Constructor of the CNMF method
 
