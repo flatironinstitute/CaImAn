453081cbf6196421209e3d2af7cb93e04eaa64b7
Pat Gunn <pgunn01@gmail.com>
2018-03-22 16:12:03 -0400

    Gah

--- caiman/mmapping.py
+++ caiman/mmapping.py
@@ -305,7 +305,6 @@ def save_place_holder(pars):
 def save_memmap(filenames, base_name='Yr', resize_fact=(1, 1, 1), remove_init=0, idx_xy=None,
                 order='F', xy_shifts=None, is_3D=False, add_to_movie=0, border_to_0=0, dview = None,
                 n_chunks=100):
-                n_chunks=100):
 
     """ Efficiently write data from a list of tif files into a memory mappable file
 
