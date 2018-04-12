7651ff324f9dc9be70f2a41b04166f85ca3197d0
Pat Gunn <pgunn01@gmail.com>
2018-04-12 17:24:49 -0400

    Don't run xvfb-run on OSX

--- test_demos.sh
+++ test_demos.sh
@@ -14,10 +14,19 @@
 
 # Make sure the xvfb-run command exists before starting demos, so we can give a better
 # error message
-command -v xvfb-run
-if [ $? != 0 ]; then
-	echo "xvfb-run command not found"
-	exit 1
+XVFB=""
+OS=$(uname -s)
+
+# On Linux we wrap our command with xvfb-run to allow X things to happen
+# and make them effectively no-ops. Not necessary on other platforms.
+
+if [ $OS == "Linux" ]; then
+	command -v xvfb-run
+	if [ $? != 0 ]; then
+		echo "xvfb-run command not found"
+		exit 1
+	fi
+	XVFB="xvfb-run -s \"-screen 0 800x600x16\" "
 fi
 
 # Tell matplotlib to try to plot less to begin with by specifying a postscript backend
@@ -32,7 +41,7 @@ for demo in demos/general/*; do
 		true
 	else
 		echo Testing demo [$demo]
-		xvfb-run -s "-screen 0 800x600x16" python $demo
+		$XVFB python $demo
 		err=$?
 		if [ $err != 0 ]; then
 			echo "	Tests failed with returncode $err"
