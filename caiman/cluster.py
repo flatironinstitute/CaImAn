1feb6096a1815878d9f28286c1ebe3e926f5929f
Pat Gunn <pgunn01@gmail.com>
2018-05-03 16:24:26 -0400

    Followup to PR 208, further restrict scope of change

--- caiman/cluster.py
+++ caiman/cluster.py
@@ -421,7 +421,17 @@ def setup_cluster(backend='multiprocessing', n_processes=None, single_thread=Fal
                 raise Exception(
                     'A cluster is already runnning. Terminate with dview.terminate() if you want to restart.')
             if (platform.system() == 'Darwin') and (sys.version_info > (3, 0)):
-                multiprocessing.set_start_method('forkserver', force=True)
+                try:
+                    if 'kernel' in get_ipython().trait_names(): # If you're on OSX and you're running under Jupyter or Spyder,
+                                                                # which already run the code in a forkserver-friendly way, this
+                                                                # can eliminate some setup and make this a reasonable approach.
+                                                                # Otherwise, seting VECLIB_MAXIMUM_THREADS=1 or using a different
+                                                                # blas/lapack is the way to avoid the issues.
+                                                                # See https://github.com/flatironinstitute/CaImAn/issues/206 for more
+                                                                # info on why we're doing this (for now).
+                        multiprocessing.set_start_method('forkserver', force=True)
+                except: # If we're not running under ipython, don't do anything.
+                    pass
             c = None
             
             dview = Pool(n_processes)
