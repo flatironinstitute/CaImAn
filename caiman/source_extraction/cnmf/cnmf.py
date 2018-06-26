fix online update of residual variance

caiman/source_extraction/cnmf/cnmf.py
@@ -836,7 +836,9 @@ class CNMF(object):
         self.Yres_buf = self.Yres_buf
         self.mean_buff = self.Yres_buf.mean(0)
         self.ind_new = []
-        self.rho_buf = imblur(np.maximum(self.Yres_buf.T, 0).reshape(
+        # self.rho_buf = imblur(np.maximum(self.Yres_buf.T, 0).reshape(
+        #     self.dims2 + (-1,), order='F'), sig=self.gSig, siz=self.gSiz, nDimBlur=len(self.dims2))**2
+        self.rho_buf = imblur(self.Yres_buf.T.reshape(
             self.dims2 + (-1,), order='F'), sig=self.gSig, siz=self.gSiz, nDimBlur=len(self.dims2))**2
         self.rho_buf = np.reshape(
             self.rho_buf, (np.prod(self.dims2), -1)).T
@@ -995,7 +997,7 @@ class CNMF(object):
 
             res_frame = np.reshape(res_frame, self.dims2, order='F')
 
-            rho = imblur(np.maximum(res_frame, 0), sig=self.gSig,
+            rho = imblur(res_frame, sig=self.gSig,
                          siz=self.gSiz, nDimBlur=len(self.dims2))**2
 
             rho = np.reshape(rho, np.prod(self.dims2))

caiman/source_extraction/cnmf/online_cnmf.py
@@ -735,17 +735,24 @@ def update_num_components(t, sv, Ab, Cf, Yres_buf, Y_buf, rho_buf,
     sv += rho_buf.get_last_frames(1).squeeze()
     sv = np.maximum(sv, 0)
 
-    # if t % 50 == 0:
-    #     plt.figure(figsize=(20, 4))
-    #     plt.subplot(141)
-    #     plt.imshow(sv.reshape(dims))
-    #     plt.subplot(142)
-    #     plt.imshow(Yres_buf.mean(0).reshape(dims, order='F'))
-    #     plt.subplot(143)
-    #     plt.imshow((Yres_buf**2).mean(0).reshape(dims, order='F'))
-    #     plt.subplot(144)
-    #     plt.imshow(Ab[:, -1].toarray().reshape(dims, order='F'))
-    #     plt.show()
+    # sv = (np.array([imblur(vb.reshape(dims, order='F'), sig=gSig, siz=gSiz, nDimBlur=len(dims))
+    #                 for vb in Yres_buf])**2).mean(0).ravel()
+
+    if t % 100 == 0:
+        plt.figure(figsize=(20, 4))
+        plt.subplot(141)
+        plt.colorbar(plt.imshow(sv.reshape(dims)))
+        plt.subplot(142)
+        plt.colorbar(plt.imshow(rho_buf.sum(0).reshape(dims)))
+        plt.subplot(143)
+        # plt.colorbar(plt.imshow((Yres_buf**2).mean(0).reshape(dims, order='F')))
+        plt.colorbar(plt.imshow((np.stack([
+            imblur(vb.reshape(dims, order='F'),
+                   sig=gSig, siz=gSiz, nDimBlur=len(dims))
+            for vb in Yres_buf])**2).sum(0)))
+        plt.subplot(144)
+        plt.colorbar(plt.imshow(Ab[:, -1].toarray().reshape(dims, order='F')))
+        plt.show()
 
     Ains, Cins, Cins_res, inds, ijsig_all, cnn_pos, local_max = get_candidate_components(
         sv, dims, Yres_buf=Yres_buf, min_num_trial=min_num_trial, gSig=gSig,
@@ -839,11 +846,13 @@ def update_num_components(t, sv, Ab, Cf, Yres_buf, Y_buf, rho_buf,
             num_added += 1
             ind_new.append(ijSig)
 
-            # plt.figure(figsize=(15,4))
-            # plt.subplot(131)
-            # plt.imshow(sv.reshape(dims))
-            # plt.subplot(132)
-            # plt.imshow(Ain[:,0].reshape(dims, order='F'))           
+            plt.figure(figsize=(20,4))
+            plt.subplot(141)
+            plt.imshow(sv.reshape(dims))
+            plt.subplot(142)
+            plt.imshow(rho_buf.mean(0).reshape(dims))
+            plt.subplot(143)
+            plt.imshow(Ain[:,0].reshape(dims, order='F'))           
 
             if oases is not None:
                 if not useOASIS:
@@ -915,33 +924,39 @@ def update_num_components(t, sv, Ab, Cf, Yres_buf, Y_buf, rho_buf,
             slices = tuple(slice(max(0, ijs[0] - sg), min(d, ijs[1] + sg))
                            for ijs, sg, d in zip(ijSig, gSiz, dims))
 
-            slice_within = tuple(slice(ijs[0] - sl.start, ijs[1] - sl.start)
-                           for ijs, sl in zip(ijSig, slices))
+            # slice_within = tuple(slice(ijs[0] - sl.start, ijs[1] - sl.start)
+            #                for ijs, sl in zip(ijSig, slices))
 
 
-            ind_vb = np.ravel_multi_index(
-               np.ix_(*[np.arange(ij[0], ij[1])
-                      for ij in ijSig]), dims, order='C').ravel()
+            # ind_vb = np.ravel_multi_index(
+            #    np.ix_(*[np.arange(ij[0], ij[1])
+            #           for ij in ijSig]), dims, order='C').ravel()
 
 
+            # vb_buf = [np.maximum(0, imblur(vb.reshape(dims,order='F')[slices][slice_within], 
+            #     sig=gSig, siz=gSiz, nDimBlur=len(dims))) for vb in Yres_buf]
 
-
-            vb_buf = [imblur(np.maximum(0,vb.reshape(dims,order='F')[slices][slice_within]), sig=gSig, siz=gSiz, nDimBlur=len(dims)) for vb in Yres_buf]
-
-            vb_buf2 = np.stack([vb.ravel() for vb in vb_buf])
+            # vb_buf2 = np.stack([vb.ravel() for vb in vb_buf])     
 
 #            ind_vb = np.ravel_multi_index(
 #                    np.ix_(*[np.arange(s.start, s.stop)
 #                           for s in slices_small]), dims).ravel()
 
-            rho_buf[:, ind_vb] = vb_buf2**2
+            ind_vb = np.ravel_multi_index(
+                np.ix_(*[np.arange(sl.start, sl.stop)
+                         for sl in slices]), dims, order='C').ravel()
 
+            # rho_buf[:, ind_vb] = vb_buf2**2
+
+            rho_buf[:, ind_vb] = np.stack([imblur(
+                vb.reshape(dims, order='F')[slices], sig=gSig, siz=gSiz,
+                nDimBlur=len(dims)).ravel() for vb in Yres_buf])**2
 
             sv[ind_vb] = np.sum(rho_buf[:, ind_vb], 0)
 
-            # plt.subplot(133)
-            # plt.imshow(sv.reshape(dims))
-            # plt.show()
+            plt.subplot(144)
+            plt.imshow(sv.reshape(dims))
+            plt.show()
 
 #            sv = np.sum([imblur(vb.reshape(dims,order='F'), sig=gSig, siz=gSiz, nDimBlur=len(dims))**2 for vb in Yres_buf], 0).reshape(-1)
 #            plt.subplot(1,5,4)