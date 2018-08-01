fixes for online experiments ran with following results #03.00. #{'recall': 0.7192982456140351, 'precision': 0.80392156862745101, 'accuracy': 0.61194029850746268, 'f1_score': 0.7592592592592593} #04.00.t #{'recall': 0.625, 'precision': 0.70135746606334837, 'accuracy': 0.49363057324840764, 'f1_score': 0.66098081023454158} #02.00 #{'recall': 0.75537, 'precision': 0.83252, 'accuracy': 0.65527, 'f1_score': 0.79199} #YUSTE #{'recall': 0.73096, 'precision': 0.8623, 'accuracy': 0.65479, 'f1_score': 0.79102} #00.00 #{'recall': 0.58157894736842108, 'precision': 0.86328125, 'accuracy': 0.53253012048192772, 'f1_score': 0.69496855345911945} #01.01 #{'recall': 0.62048192771084343, 'precision': 0.8046875, 'accuracy': 0.53926701570680624, 'f1_score': 0.70068027210884354} #k53 #{'recall': 0.7814269535673839, 'precision': 0.88917525773195871, 'accuracy': 0.71207430340557276, 'f1_score': 0.83182640144665465} #J115 #{'recall': 0.77508650519031141, 'precision': 0.84105131414267831, 'accuracy': 0.676056338028169, 'f1_score': 0.80672268907563027} #J123 #{'recall': 0.6795580110497238, 'precision': 0.93181818181818177, 'accuracy': 0.64736842105263159, 'f1_score': 0.78594249201277955}

caiman/source_extraction/cnmf/online_cnmf.py
@@ -541,7 +541,7 @@ class OnACID(object):
 
         ds_factor = np.maximum(opts['ds_factor'], 1)
         if ds_factor > 1:
-            Y.resize(1./ds_factor)
+            Y = Y.resize(1./ds_factor, 1./ds_factor)
         mc_flag = self.params.get('online', 'motion_correct')
         self.estimates.shifts = []  # store motion shifts here
         self.estimates.time_new_comp = []
@@ -619,6 +619,7 @@ class OnACID(object):
         else:
             raise Exception('Unknown initialization method!')
         dims, Ts = get_file_size(fls)
+        dims = Y.shape[1:]
         self.params.set('data', {'dims': dims})
         T1 = np.array(Ts).sum()*self.params.get('online', 'epochs')
         self._prepare_object(Yr, T1)

use_cases/CaImAnpaper/scripts_paper/Preprocessing_online_refactor.py
@@ -64,7 +64,7 @@ try:
     save_results = True
 
 except:
-    ID = [0, 3]  # range(6,9)
+    ID = range(9)
     print('ID NOT PASSED')
     plot_on = False
     save_results = False
@@ -72,7 +72,7 @@ except:
 preprocessing_from_scratch = True
 if preprocessing_from_scratch:
     reload = False
-    save_results = False  # set to True if you want to regenerate
+    save_results = True  # set to True if you want to regenerate
 
 mot_corr = False
 base_folder = '/mnt/ceph/users/agiovann/LinuxDropbox/Dropbox/DATA_PAPER_ELIFE/'
@@ -361,6 +361,7 @@ if preprocessing_from_scratch:
         min_size_neuro = min_radius ** 2 * np.pi
         max_size_neuro = max_radius ** 2 * np.pi
 
+
         # %%
         c, dview, n_processes = cm.cluster.setup_cluster(backend='local', n_processes=None, single_thread=True)
         gt_file = os.path.join(base_folder, os.path.split(params_movie[ind_dataset]['fname'])[0],
@@ -373,16 +374,15 @@ if preprocessing_from_scratch:
             C_gt = ld['C_gt']
             Cn_orig = ld['Cn']
             if ds_factor > 1:
-                A_gt = cm.movie(np.reshape(A_gt, dims_or + (-1,), order='F')).transpose(2, 0, 1).resize(1. / ds_factor,
-                                                                                                        1. / ds_factor)
-                A_gt2 = np.array(np.reshape(A_gt, (A_gt.shape[0], -1), order='F')).T
-                Cn_orig = cv2.resize(Cn_orig, None, fx=1. / ds_factor, fy=1. / ds_factor)
+                A_gt2= np.concatenate([cv2.resize(A_gt[:, fr_].reshape(dims_or, order='F'), cnm.dims[::-1]).reshape(-1, order='F')[:,None] for fr_ in range(A_gt.shape[-1])], axis = 1)
+                # A_gt2 = np.reshape(A_gt, (A_gt.shape[0],-1), order='F')
+                Cn_orig = cv2.resize(Cn_orig, cnm.dims[::-1])
             else:
                 A_gt2 = A_gt.copy()
 
         # A_gt_thr = cm.source_extraction.cnmf.spatial.threshold_components(A_gt2, dims, medw=None, thr_method='max', maxthr=global_params['max_thr'], extract_cc=True,
         #                          se=None, ss=None, dview=None)
-        gt_estimate = Estimates(A=scipy.sparse.csc_matrix(A_gt2), b=None, C=C_gt, f=None, R=None, dims=dims_or)
+        gt_estimate = Estimates(A=scipy.sparse.csc_matrix(A_gt2), b=None, C=C_gt, f=None, R=None, dims=cnm.dims)
         gt_estimate.threshold_spatial_components(maxthr=global_params['max_thr'], dview=dview)
         gt_estimate.remove_small_large_neurons(min_size_neuro, max_size_neuro)
         _ = gt_estimate.remove_duplicates(predictions=None, r_values=None, dist_thr=0.1, min_dist=10, thresh_subset=0.6)
@@ -446,7 +446,6 @@ if preprocessing_from_scratch:
 
 else:
     print('**** LOADING PREPROCESSED RESULTS ****')
-    lsls
     with np.load('RESULTS_SUMMARY/all_results_Jan_2018_online.npz') as ld:
         all_results = ld['all_results'][()]
 