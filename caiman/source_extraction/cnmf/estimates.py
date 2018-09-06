b51b638c70ff12beb09c96adce5caf4b3e2d899e
epnev <eap2111@columbia.edu>
2018-09-06 11:53:59 -0400

    issue 366

--- caiman/source_extraction/cnmf/estimates.py
+++ caiman/source_extraction/cnmf/estimates.py
@@ -578,7 +578,10 @@ class Estimates(object):
             nB_mat = scipy.sparse.spdiags(nB, 0, nB.shape[0], nB.shape[0])
             nB_inv_mat = scipy.sparse.spdiags(1. / nB, 0, nB.shape[0], nB.shape[0])
             self.b = self.b * nB_inv_mat
-            self.f = nB_mat * self.f
+            if max(nB_mat.shape) == 1:
+                self.f = nB_mat.diagonal()[0] * self.f
+            else:
+                self.f = nB_mat * self.f
         return self
 
     def select_components(self, idx_components=None, use_object=False):
