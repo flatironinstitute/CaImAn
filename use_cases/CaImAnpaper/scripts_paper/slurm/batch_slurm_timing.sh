371fdc23ec641ad60667cb56bff742dfc9f1005b
Andrea Giovannucci <andrea.giovannucci@gmail.com>
2018-10-09 18:08:57 -0400

    slurm scripts for paper in the repo

---
+++ use_cases/CaImAnpaper/scripts_paper/slurm/batch_slurm_timing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run 21 tasks, with ids 0..20. Have seven on a node 
+# The stdout (-o) and stderr (-e) files will be written to the submission directory with
+# names that follow the given templates (in this case, the names will include the
+# SLURM job identifier).
+#SBATCH -p skytest -N9 --ntasks-per-node=1 --exclusive -o sbExample.%j.out -e sbExample.%j.err
+
+# srun starts a process per task, 21 in this case. In the first example, we didn't explicitly set
+# a number of tasks, so srun assumed there would be one per node.
+srun bash -c 'KERAS_BACKEND=tensorflow CUDA_VISIBLE_DEVICES=-1 MKL_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 /mnt/home/agiovann/anaconda3/envs/caiman3/bin/python /mnt/home/agiovann//SOFTWARE/CaImAn/use_cases/CaImAnpaper/scripts_paper/batch_evaluate_timing.py $SLURM_PROCID'
