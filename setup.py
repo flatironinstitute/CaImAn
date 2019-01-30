05795a20070ed3d757e88846ede9ee79df70afb0
Pat Gunn <pgunn01@gmail.com>
2019-01-30 17:34:58 -0500

    Forgot an import

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 import os
 from os import path
+import sys
 import numpy as np
 from Cython.Build import cythonize
 from setuptools.extension import Extension
