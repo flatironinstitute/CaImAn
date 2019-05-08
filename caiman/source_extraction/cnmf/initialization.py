fix some numerical issues, and enable p=0

caiman/source_extraction/cnmf/initialization.py
@@ -1530,7 +1530,8 @@ def extract_ac(data_filtered, data_raw, ind_ctr, patch_dims):
     XX = np.dot(X.T, X)
     Xy = np.dot(X.T, data_raw)
     try:
-        ai = np.linalg.inv(XX).dot(Xy)[0]
+        #ai = np.linalg.inv(XX).dot(Xy)[0]
+        ai = np.linalg.solve(XX, Xy)[0]
     except:
         ai = scipy.linalg.lstsq(XX, Xy)[0][0]
     ai = ai.reshape(patch_dims)
@@ -1658,7 +1659,8 @@ def compute_W(Y, A, C, dims, radius, data_fits_in_memory=True, ssub=1, tsub=1, p
                     downscale(C, (1, tsub))) if A.size > 0 else 0) - \
                 downscale(b0.reshape(dims, order='F'),
                           (ssub, ssub)).reshape((-1, 1), order='F')[p]
-        data = np.linalg.inv(tmp).dot(B.dot(tmp2))
+        #data = np.linalg.inv(tmp).dot(B.dot(tmp2))
+        data = np.linalg.solve(tmp, B.dot(tmp2))
         return index, data
 
     Q = list((parmap if parallel else map)(process_pixel, range(len(X))))

caiman/source_extraction/cnmf/online_cnmf.py
@@ -453,7 +453,12 @@ class OnACID(object):
             self.estimates.rho_buf.append(rho)
 
             # old_max_img = self.estimates.max_img.copy()
-
+            if self.params.get('preprocess', 'p') == 1:
+                g_est = np.mean(self.estimates.g)
+            elif self.params.get('preprocess', 'p') == 2:
+                g_est = np.mean(self.estimates, 0)
+            else:
+                g_est = 0
             use_corr = self.params.get('online', 'use_corr_img')
             (self.estimates.Ab, Cf_temp, self.estimates.Yres_buf, self.estimates.rho_buf,
                 self.estimates.CC, self.estimates.CY, self.ind_A, self.estimates.sv,
@@ -469,8 +474,7 @@ class OnACID(object):
                 thresh_overlap=self.params.get('online', 'thresh_overlap'), groups=self.estimates.groups,
                 batch_update_suff_stat=self.params.get('online', 'batch_update_suff_stat'),
                 gnb=self.params.get('init', 'nb'), sn=self.estimates.sn, 
-                g=(np.mean(self.estimates.g) if self.params.get('preprocess', 'p') == 1 else 
-                    np.mean(self.estimates.g, 0)), s_min=self.params.get('temporal', 's_min'),
+                g=g_est, s_min=self.params.get('temporal', 's_min'),
                 Ab_dense=self.estimates.Ab_dense if self.params.get('online', 'use_dense') else None,
                 oases=self.estimates.OASISinstances if self.params.get('preprocess', 'p') else None,
                 N_samples_exceptionality=self.params.get('online', 'N_samples_exceptionality'),