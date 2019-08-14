1c036a1ff41affcdff062431e448825104e9e78a
HagaiHargil <hagaihargil@gmail.com>
2019-08-14 09:12:21 +0300

    Fixed merge artifact

--- caiman/mmapping.py
+++ caiman/mmapping.py
@@ -35,6 +35,7 @@ def prepare_shape(mytuple: Tuple) -> Tuple:
     return tuple(map(lambda x: np.uint64(x), mytuple))
 
 
+    
 #%%
 def load_memmap(filename: str, mode: str = 'r') -> Tuple[Any, Tuple, int]:
     """ Load a memory mapped file created by the function save_memmap
@@ -60,36 +61,20 @@ def load_memmap(filename: str, mode: str = 'r') -> Tuple[Any, Tuple, int]:
         ValueError "Unknown file extension"
 
     """
-    if ('.mmap' in filename):
-        # Strip path components and use CAIMAN_DATA/example_movies
-        # TODO: Eventually get the code to save these in a different dir
-        file_to_load = filename
-        filename = os.path.split(filename)[-1]
-        fpart = filename.split('_')[1:-1]      # The filename encodes the structure of the map
-        d1, d2, d3, T, order = int(fpart[-9]), int(fpart[-7]), int(fpart[-5]), int(fpart[-1]), fpart[-3]
-        Yr = np.memmap(file_to_load, mode=mode, shape=prepare_shape((d1 * d2 * d3, T)), dtype=np.float32, order=order)
-        if d3 == 1:
-            return (Yr, (d1, d2), T)
-        else:
-            return (Yr, (d1, d2, d3), T)
-    else:
+    if pathlib.Path(filename).suffix != '.mmap':
         logging.error("Unknown extension for file " + str(filename))
         raise ValueError('Unknown file extension (should be .mmap)')
-
     # Strip path components and use CAIMAN_DATA/example_movies
     # TODO: Eventually get the code to save these in a different dir
     file_to_load = filename
     filename = os.path.split(filename)[-1]
-    fpart = filename.split('_')[1:-1] # The filename encodes the structure of the map
-    d1, d2, d3, T, order = int(fpart[-9]), int(fpart[-7]
-                                                ), int(fpart[-5]), int(fpart[-1]), fpart[-3]
-    Yr = np.memmap(file_to_load, mode=mode, shape=prepare_shape((
-        d1 * d2 * d3, T)), dtype=np.float32, order=order)
+    fpart = filename.split('_')[1:-1]      # The filename encodes the structure of the map
+    d1, d2, d3, T, order = int(fpart[-9]), int(fpart[-7]), int(fpart[-5]), int(fpart[-1]), fpart[-3]
+    Yr = np.memmap(file_to_load, mode=mode, shape=prepare_shape((d1 * d2 * d3, T)), dtype=np.float32, order=order)
     if d3 == 1:
         return (Yr, (d1, d2), T)
     else:
         return (Yr, (d1, d2, d3), T)
-
 
 
 #%%
