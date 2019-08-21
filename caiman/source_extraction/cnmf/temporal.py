flatironinstitute/CaImAn

commit 0ec3b17f388ef445f481288fcbca483cccf2d3ab

using lasso for spatial components(?)

caiman/source_extraction/cnmf/temporal.py
@@ -9,7 +9,7 @@ from builtins import str
 from builtins import map
 from builtins import range
 import logging
-from scipy.sparse import spdiags, diags, coo_matrix  # ,csgraph
+from scipy.sparse import spdiags, diags, coo_matrix, csc_matrix  # ,csgraph
 import scipy
 import numpy as np
 import platform
@@ -194,7 +194,7 @@ def update_temporal_components(Y, A, b, Cin, fin, bl=None, c1=None, g=None, sn=N
     A = scipy.sparse.hstack((A, b)).tocsc()
     S = np.zeros(np.shape(Cin))
     Cin = np.vstack((Cin, fin))
-    C = Cin.copy()
+    #C = Cin.copy()
     nA = np.ravel(A.power(2).sum(axis=0))
 
     logging.info('Generating residuals')
@@ -203,15 +203,13 @@ def update_temporal_components(Y, A, b, Cin, fin, bl=None, c1=None, g=None, sn=N
         YA = parallel_dot_product(Y, A, dview=dview, block_size=block_size_temp,
                                   transpose=True, num_blocks_per_run=num_blocks_per_run_temp) * diags(1. / nA)
     else:
-        # YA = (A.T.dot(Y).T) * spdiags(old_div(1., nA), 0, nr + nb, nr + nb)
         YA = (A.T.dot(Y).T) * diags(1. / nA)
-    # AA = ((A.T.dot(A)) * spdiags(old_div(1., nA), 0, nr + nb, nr + nb)).tocsr()
     AA = ((A.T.dot(A)) * diags(1. / nA)).tocsr()
     YrA = YA - AA.T.dot(Cin).T
     # creating the patch of components to be computed in parrallel
     parrllcomp, len_parrllcomp = update_order_greedy(AA[:nr, :][:, :nr])
     logging.info("entering the deconvolution ")
-    C, S, bl, YrA, c1, sn, g, lam = update_iteration(parrllcomp, len_parrllcomp, nb, C, S, bl, nr,
+    C, S, bl, YrA, c1, sn, g, lam = update_iteration(parrllcomp, len_parrllcomp, nb, Cin, S, bl, nr,
                                                      ITER, YrA, c1, sn, g, Cin, T, nA, dview, debug, AA, kwargs)
     ff = np.where(np.sum(C, axis=1) == 0)  # remove empty components
     if np.size(ff) > 0:  # Eliminating empty temporal components
@@ -235,7 +233,7 @@ def update_temporal_components(Y, A, b, Cin, fin, bl=None, c1=None, g=None, sn=N
         nr = nr - (len(ff) - len(background_ff))
 
     b = A[:, nr:].toarray()
-    A = coo_matrix(A[:, :nr])
+    A = csc_matrix(A[:, :nr])
     f = C[nr:, :]
     C = C[:nr, :]
     YrA = np.array(YrA[:, :nr]).T

caiman/source_extraction/cnmf/spatial.py
@@ -220,7 +220,6 @@ def update_spatial_components(Y, C=None, f=None, A_in=None, sn=None, dims=None,
     if i + n_pixels_per_process < np.prod(dims):
         pixel_groups.append([Y_name, C_name, sn, ind2_[(i + n_pixels_per_process):np.prod(dims)], list(
             range(i + n_pixels_per_process, np.prod(dims))), method_ls, cct])
-    #A_ = np.zeros((d, nr + np.size(f, 0)))  # init A_
     #A_ = scipy.sparse.lil_matrix((d, nr + np.size(f, 0)))
     if dview is not None:
         if 'multiprocessing' in str(type(dview)):
@@ -384,7 +383,6 @@ def regression_ipyparallel(pars):
 
     _, T = np.shape(C)  # initialize values
     As = []
-
     for y, px, idx_px_from_0 in zip(Y, idxs_Y, range(len(idxs_C))):
         c = C[idxs_C[idx_px_from_0], :]
         idx_only_neurons = idxs_C[idx_px_from_0]
@@ -405,7 +403,11 @@ def regression_ipyparallel(pars):
             elif method_least_square == 'lasso_lars':  # lasso lars function from scikit learn
                 lambda_lasso = 0 if np.size(cct_) == 0 else \
                     .5 * noise_sn[px] * np.sqrt(np.max(cct_)) / T
-                clf = linear_model.LassoLars(alpha=lambda_lasso, positive=True, fit_intercept=True)
+                clf = linear_model.LassoLars(alpha=lambda_lasso, positive=True,
+                                             fit_intercept=True)
+#                clf = linear_model.Lasso(alpha=lambda_lasso, positive=True,
+#                                         fit_intercept=True, normalize=True,
+#                                         selection='random')
                 a_lrs = clf.fit(np.array(c.T), np.ravel(y))
                 a = a_lrs.coef_
 

commit ddf9b048fa4da1513a230a516bfb6ea30dd098c5

reverting a bug introduced with 0ec3b17f388ef445f481288fcbca483cccf2d3ab

caiman/source_extraction/cnmf/temporal.py
@@ -195,7 +195,7 @@ def update_temporal_components(Y, A, b, Cin, fin, bl=None, c1=None, g=None, sn=N
     A = scipy.sparse.hstack((A, b)).tocsc()
     S = np.zeros(np.shape(Cin))
     Cin = np.vstack((Cin, fin))
-    #C = Cin.copy()
+    C = Cin.copy()
     nA = np.ravel(A.power(2).sum(axis=0))
 
     logging.info('Generating residuals')
@@ -213,7 +213,7 @@ def update_temporal_components(Y, A, b, Cin, fin, bl=None, c1=None, g=None, sn=N
     # creating the patch of components to be computed in parrallel
     parrllcomp, len_parrllcomp = update_order_greedy(AA[:nr, :][:, :nr])
     logging.info("entering the deconvolution ")
-    C, S, bl, YrA, c1, sn, g, lam = update_iteration(parrllcomp, len_parrllcomp, nb, Cin, S, bl, nr,
+    C, S, bl, YrA, c1, sn, g, lam = update_iteration(parrllcomp, len_parrllcomp, nb, C, S, bl, nr,
                                                      ITER, YrA, c1, sn, g, Cin, T, nA, dview, debug, AA, kwargs)
     ff = np.where(np.sum(C, axis=1) == 0)  # remove empty components
     if np.size(ff) > 0:  # Eliminating empty temporal components