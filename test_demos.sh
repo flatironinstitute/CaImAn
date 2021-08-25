33b21e994a4f7b4c2daa9777a81bd219618ce306
Pat Gunn <pgunn@flatironinstitute.org>
2021-08-25 13:06:00 -0400

    test_demos: fix typo

--- test_demos.sh
+++ test_demos.sh
@@ -42,7 +42,7 @@ for demo in demos/general/*; do
 		echo "	Skipping tests on $demo: This is interactive"
 	elif [ $demo == "demos/general/demo_pipeline_voltage_imaging.py" ]; then
 		echo "	Skipping tests on $demo: This uses Keras, an optional install"
-	elif [ $demo == "demos/general/demo_pipeline_NWB" ]; then
+	elif [ $demo == "demos/general/demo_pipeline_NWB.py" ]; then
 		echo "	Skipping tests on $demo: Skip for interactivity"
 	elif [ -d $demo ]; then
 		true
