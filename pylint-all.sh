c321c4fcc65ecbdd2ff5823c43e3bca395912c7d
Pat Gunn <pgunn01@gmail.com>
2023-03-30 11:45:11 -0400

    Remove use_cases from main repo; document where to find it.

--- pylint-all.sh
+++ pylint-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name "*.py" -not -path "./use_cases/*" -not -path "./sandbox/*" -not -path "./caiman/source_extraction/*" -exec pylint --msg-template='{path}:{line}: [{msg_id}:{symbol}] {msg}' -s n \{} \;
+find . -name "*.py" -not -path "./sandbox/*" -not -path "./caiman/source_extraction/*" -exec pylint --msg-template='{path}:{line}: [{msg_id}:{symbol}] {msg}' -s n \{} \;
