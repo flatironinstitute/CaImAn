896389a861ee9e90c79422aea2a489c9c6899788
Pat Gunn <pgunn01@gmail.com>
2024-04-12 15:06:08 -0400

    demo_pipeline_cnmfE.py: ready for testing

--- demos/general/demo_pipeline_cnmfE.py
+++ demos/general/demo_pipeline_cnmfE.py
@@ -9,10 +9,7 @@ the case of 2p data processing with core difference being the usage of the
 CNMF-E algorithm for source extraction (as opposed to plain CNMF). Check
 the companion paper for more details.
 
-You can also run a large part of the pipeline with a single method
-(cnmf.fit_file) See inside for details.
-
-Demo is also available as a jupyter notebook (see demo_pipeline_cnmfE.ipynb)
+See also the jupyter notebook demo_pipeline_cnmfE.ipynb
 """
 
 import argparse
@@ -45,54 +42,28 @@ def main():
             "[%(filename)s:%(funcName)20s():%(lineno)s] %(message)s",
             level=logging.INFO)
 
-    if cfg.input is None:
+    opts = params.CNMFParams(params_from_file=cfg.configfile)
+
+    if cfg.input is not None:
         # If no input is specified, use sample data, downloading if necessary
+        opts.change_params({"data": {"fnames": cfg.input}})
+    if not opts.data['fnames']: # Set neither by CLI arg nor through JSON, so use default data
         fnames = [download_demo('data_endoscope.tif')]
-    else:
-        fnames = cfg.input
-    filename_reorder = fnames # XXX What is this?
+        opts.change_params({"data": {"fnames": fnames}})
 
     # First set up some parameters for data and motion correction
     # dataset dependent parameters
-    fr = 10                          # movie frame rate
-    decay_time = 0.4                 # length of a typical transient in seconds
-
-    # motion correction parameters
-    motion_correct = True            # flag for motion correction
-    pw_rigid = False                 # flag for pw-rigid motion correction
-
-    gSig_filt = (3, 3)   # size of filter, in general gSig (see below),
-    #                      change this one if algorithm does not work
-    max_shifts = (5, 5)  # maximum allowed rigid shift
-    strides = (48, 48)   # start a new patch for pw-rigid motion correction every x pixels
-    overlaps = (24, 24)  # overlap between patches (size of patch strides+overlaps)
-    # maximum deviation allowed for patch with respect to rigid shifts
-    max_deviation_rigid = 3
-    border_nan = 'copy'
-
-    params_dict = {'fnames': fnames,
-                   'fr': fr,
-                   'decay_time': decay_time,
-                   'pw_rigid': pw_rigid,
-                   'max_shifts': max_shifts,
-                   'gSig_filt': gSig_filt,
-                   'strides': strides,
-                   'overlaps': overlaps,
-                   'max_deviation_rigid': max_deviation_rigid,
-                   'border_nan': border_nan}
-
-    opts = params.CNMFParams(params_dict=params_dict)
 
     c, dview, n_processes = caiman.cluster.setup_cluster(backend=cfg.cluster_backend, n_processes=cfg.cluster_nproc)
     # Motion Correction
-    #  The pw_rigid flag set above, determines where to use rigid or pw-rigid
+    #  The motion:pw_rigid parameter determines where to use rigid or pw-rigid
     #  motion correction
-    if motion_correct:
+    if not cfg.no_motion_correct:
         # do motion correction rigid
-        mc = MotionCorrect(fnames, dview=dview, **opts.get_group('motion'))
+        mc = MotionCorrect(opts.data['fnames'], dview=dview, **opts.get_group('motion'))
         mc.motion_correct(save_movie=True)
-        fname_mc = mc.fname_tot_els if pw_rigid else mc.fname_tot_rig
-        if pw_rigid:
+        fname_mc = mc.fname_tot_els if opts.motion['pw_rigid'] else mc.fname_tot_rig
+        if opts.motion['pw_rigid']:
             bord_px = np.ceil(np.maximum(np.max(np.abs(mc.x_shifts_els)),
                                          np.max(np.abs(mc.y_shifts_els)))).astype(int)
         else:
@@ -107,7 +78,7 @@ def main():
         fname_new = caiman.save_memmap(fname_mc, base_name='memmap_', order='C',
                                    border_to_0=bord_px)
     else:  # if no motion correction just memory map the file
-        fname_new = caiman.save_memmap(filename_reorder, base_name='memmap_',
+        fname_new = caiman.save_memmap(opts.data['fnames'], base_name='memmap_',
                                    order='C', border_to_0=0, dview=dview)
 
     # load memory mappable file
@@ -115,95 +86,33 @@ def main():
     images = Yr.T.reshape((T,) + dims, order='F')
 
     # Parameters for source extraction and deconvolution (CNMF-E algorithm)
-    p = 1               # order of the autoregressive system
-    K = None            # upper bound on number of components per patch, in general None for 1p data
-    gSig = (3, 3)       # gaussian width of a 2D gaussian kernel, which approximates a neuron
-    gSiz = (13, 13)     # average diameter of a neuron, in general 4*gSig+1
     Ain = None          # possibility to seed with predetermined binary masks
-    merge_thr = .7      # merging threshold, max correlation allowed
-    rf = 40             # half-size of the patches in pixels. e.g., if rf=40, patches are 80x80
-    stride_cnmf = 20    # amount of overlap between the patches in pixels
-    #                     (keep it at least large as gSiz, i.e 4 times the neuron size gSig)
-    tsub = 2            # downsampling factor in time for initialization,
-    #                     increase if you have memory problems
-    ssub = 1            # downsampling factor in space for initialization,
-    #                     increase if you have memory problems
-    #                     you can pass them here as boolean vectors
-    low_rank_background = None  # None leaves background of each patch intact,
-    #                     True performs global low-rank approximation if gnb>0
-    gnb = 0             # number of background components (rank) if positive,
-    #                     else exact ring model with following settings
-    #                         gnb= 0: Return background as b and W
-    #                         gnb=-1: Return full rank background B
-    #                         gnb<-1: Don't return background
-    nb_patch = 0        # number of background components (rank) per patch if gnb>0,
-    #                     else it is set automatically
-    min_corr = .8       # min peak value from correlation image
-    min_pnr = 10        # min peak to noise ration from PNR image
-    ssub_B = 2          # additional downsampling factor in space for background
-    ring_size_factor = 1.4  # radius of ring is gSiz*ring_size_factor
 
-    opts.change_params(params_dict={'dims': dims,
-                                    'method_init': 'corr_pnr',  # use this for 1 photon
-                                    'K': K,
-                                    'gSig': gSig,
-                                    'gSiz': gSiz,
-                                    'merge_thr': merge_thr,
-                                    'p': p,
-                                    'tsub': tsub,
-                                    'ssub': ssub,
-                                    'rf': rf,
-                                    'stride': stride_cnmf,
-                                    'only_init': True,    # set it to True to run CNMF-E
-                                    'nb': gnb,
-                                    'nb_patch': nb_patch,
-                                    'method_deconvolution': 'oasis',       # could use 'cvxpy' alternatively
-                                    'low_rank_background': low_rank_background,
-                                    'update_background_components': True,  # sometimes setting to False improve the results
-                                    'min_corr': min_corr,
-                                    'min_pnr': min_pnr,
-                                    'normalize_init': False,               # just leave as is
-                                    'center_psf': True,                    # leave as is for 1 photon
-                                    'ssub_B': ssub_B,
-                                    'ring_size_factor': ring_size_factor,
-                                    'del_duplicates': True,                # whether to remove duplicates from initialization
+    opts.change_params(params_dict={'dims': dims,                          # we rework the source files
                                     'border_pix': bord_px})                # number of pixels to not consider in the borders)
 
     # compute some summary images (correlation and peak to noise)
     # change swap dim if output looks weird, it is a problem with tiffile
-    cn_filter, pnr = caiman.summary_images.correlation_pnr(images[::1], gSig=gSig[0], swap_dim=False)
+    cn_filter, pnr = caiman.summary_images.correlation_pnr(images[::1], gSig=opts.init['gSig'][0], swap_dim=False)
     # if your images file is too long this computation will take unnecessarily
     # long time and consume a lot of memory. Consider changing images[::1] to
     # images[::5] or something similar to compute on a subset of the data
 
     # inspect the summary images and set the parameters
     inspect_correlation_pnr(cn_filter, pnr)
-    # print parameters set above, modify them if necessary based on summary images
-    print(f"Minimum correlation: {min_corr}") # min correlation of peak (from correlation image)
-    print(f"Minimum peak to noise ratio: {min_pnr}")  # min peak to noise ratio
+    print(f"Minimum correlation: {opts.init['min_corr']}") # min correlation of peak (from correlation image)
+    print(f"Minimum peak to noise ratio: {opts.init['min_pnr']}")  # min peak to noise ratio
 
     # Run CMNF in patches
     cnm = cnmf.CNMF(n_processes=n_processes, dview=dview, Ain=Ain, params=opts)
     cnm.fit(images)
 
-    # ALTERNATE WAY TO RUN THE PIPELINE AT ONCE (optional -- commented out)
-    #   you can also perform the motion correction plus cnmf fitting steps
-    #   simultaneously after defining your parameters object using
-    #    cnm1 = cnmf.CNMF(n_processes, params=opts, dview=dview)
-    #    cnm1.fit_file(motion_correct=True)
-
     # Quality Control: DISCARD LOW QUALITY COMPONENTS
-    min_SNR = 2.5           # adaptive way to set threshold on the transient size
-    r_values_min = 0.85    # threshold on space consistency (if you lower more components
-    #                        will be accepted, potentially with worst quality)
-    cnm.params.set('quality', {'min_SNR': min_SNR,
-                               'rval_thr': r_values_min,
-                               'use_cnn': False})
     cnm.estimates.evaluate_components(images, cnm.params, dview=dview)
 
     print(' ***** ')
-    print('Number of total components: ', len(cnm.estimates.C))
-    print('Number of accepted components: ', len(cnm.estimates.idx_components))
+    print(f"Number of total components: {len(cnm.estimates.C)}")
+    print(f"Number of accepted components: {len(cnm.estimates.idx_components)}")
 
     # Play result movies
     if not cfg.no_play:
@@ -227,6 +136,8 @@ def main():
 
 def handle_args():
     parser = argparse.ArgumentParser(description="Demonstrate CNMFE Pipeline")
+    parser.add_argument("--configfile", default=os.path.join(caiman.paths.caiman_datadir(), 'demos', 'general', 'params_demo_pipeline_cnmfE.json'), help="JSON Configfile for Caiman parameters")
+    parser.add_argument("--no_motion_correct", action='store_true', help="Set to disable motion correction")
     parser.add_argument("--keep_logs",  action="store_true", help="Keep temporary logfiles")
     parser.add_argument("--no_play",    action="store_true", help="Do not display results")
     parser.add_argument("--cluster_backend", default="multiprocessing", help="Specify multiprocessing, ipyparallel, or single to pick an engine")
