7294d7f7d8d4271ee7cec44e8aaedbc39551053b
kushalkolar <kushalkolar@gmail.com>
2024-08-15 21:21:09 -0400

    remove more bokeh, move get_contours() to cnmf.utilities

--- caiman/source_extraction/cnmf/utilities.py
+++ caiman/source_extraction/cnmf/utilities.py
@@ -14,6 +14,8 @@ import numpy as np
 import scipy
 from scipy.sparse import spdiags, issparse, csc_matrix, csr_matrix
 import scipy.ndimage as ndi
+from skimage.measure import find_contours
+from warnings import warn
 
 import caiman.base.rois
 import caiman.cluster
@@ -1109,3 +1111,114 @@ def fast_graph_Laplacian_pixel(pars):
         ind = np.where(w>0)[0]
 
     return indices[ind].tolist(), w[ind].tolist()
+
+
+def get_contours(A, dims, thr=0.9, thr_method='nrg', swap_dim=False, slice_dim: int = None):
+    """Gets contour of spatial components and returns their coordinates
+
+     Args:
+         A:   np.ndarray or sparse matrix
+                   Matrix of Spatial components (d x K)
+
+             dims: tuple of ints
+                   Spatial dimensions of movie
+
+             thr: scalar between 0 and 1
+                   Energy threshold for computing contours (default 0.9)
+
+             thr_method: string
+                  Method of thresholding:
+                      'max' sets to zero pixels that have value less than a fraction of the max value
+                      'nrg' keeps the pixels that contribute up to a specified fraction of the energy
+
+             swap_dim: bool
+                  If False (default), each column of A should be reshaped in F-order to recover the mask;
+                  this is correct if the dimensions have not been reordered from (y, x[, z]).
+                  If True, each column should be reshaped in C-order; this is correct for dims = ([z, ]x, y).
+
+             slice_dim: int or None
+                  Which dimension to slice along if we have 3D data. (i.e., get contours on each plane along this axis).
+                  The default (None) is 0 if swap_dim is True, else -1.
+
+     Returns:
+         Coor: list of coordinates with center of mass and
+                contour plot coordinates (per layer) for each component
+    """
+
+    if 'csc_matrix' not in str(type(A)):
+        A = csc_matrix(A)
+    d, nr = np.shape(A)
+
+    coordinates = []
+
+    # get the center of mass of neurons( patches )
+    cm = caiman.base.rois.com(A, *dims, order='C' if swap_dim else 'F')
+
+    # for each patches
+    for i in range(nr):
+        pars: dict = dict()
+        # we compute the cumulative sum of the energy of the Ath component that has been ordered from least to highest
+        patch_data = A.data[A.indptr[i]:A.indptr[i + 1]]
+        indx = np.argsort(patch_data)[::-1]
+        if thr_method == 'nrg':
+            cumEn = np.cumsum(patch_data[indx] ** 2)
+            if len(cumEn) == 0:
+                pars = dict(
+                    coordinates=np.array([]),
+                    CoM=np.array([np.NaN, np.NaN]),
+                    neuron_id=i + 1,
+                )
+                coordinates.append(pars)
+                continue
+            else:
+                # we work with normalized values
+                cumEn /= cumEn[-1]
+                Bvec = np.ones(d)
+                # we put it in a similar matrix
+                Bvec[A.indices[A.indptr[i]:A.indptr[i + 1]][indx]] = cumEn
+        else:
+            if thr_method != 'max':
+                warn("Unknown threshold method. Choosing max")
+            Bvec = np.zeros(d)
+            Bvec[A.indices[A.indptr[i]:A.indptr[i + 1]]] = patch_data / patch_data.max()
+
+        if swap_dim:
+            Bmat = np.reshape(Bvec, dims, order='C')
+        else:
+            Bmat = np.reshape(Bvec, dims, order='F')
+
+        def get_slice_coords(B: np.ndarray) -> np.ndarray:
+            """Get contour coordinates for a 2D slice"""
+            d1, d2 = B.shape
+            vertices = find_contours(B.T, thr)
+            # this fix is necessary for having disjoint figures and borders plotted correctly
+            v = np.atleast_2d([np.nan, np.nan])
+            for _, vtx in enumerate(vertices):
+                num_close_coords = np.sum(np.isclose(vtx[0, :], vtx[-1, :]))
+                if num_close_coords < 2:
+                    if num_close_coords == 0:
+                        # case angle
+                        newpt = np.round(np.mean(vtx[[0, -1], :], axis=0) / [d2, d1]) * [d2, d1]
+                        vtx = np.concatenate((newpt[np.newaxis, :], vtx, newpt[np.newaxis, :]), axis=0)
+                    else:
+                        # case one is border
+                        vtx = np.concatenate((vtx, vtx[0, np.newaxis]), axis=0)
+                v = np.concatenate(
+                    (v, vtx, np.atleast_2d([np.nan, np.nan])), axis=0)
+            return v
+
+        if len(dims) == 2:
+            pars['coordinates'] = get_slice_coords(Bmat)
+        else:
+            # make a list of the contour coordinates for each 2D slice
+            pars['coordinates'] = []
+            if slice_dim is None:
+                slice_dim = 0 if swap_dim else -1
+            for s in range(dims[slice_dim]):
+                B = Bmat.take(s, axis=slice_dim)
+                pars['coordinates'].append(get_slice_coords(B))
+
+        pars['CoM'] = np.squeeze(cm[i, :])
+        pars['neuron_id'] = i + 1
+        coordinates.append(pars)
+    return coordinates
