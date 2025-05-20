11ed9b86c770db638ace4c325fc2d6e3a4a48b68
Pat Gunn <pgunn01@gmail.com>
2025-05-20 11:48:54 -0400

    Move from LooseVersion to packaging.version.Version to avoid deprecation

--- caiman/source_extraction/volpy/mrcnn/model.py
+++ caiman/source_extraction/volpy/mrcnn/model.py
@@ -26,8 +26,8 @@ import tensorflow.keras.models as KM
 from ..mrcnn import utils
 
 # Requires TensorFlow 2.0+
-from distutils.version import LooseVersion
-assert LooseVersion(tf.__version__) >= LooseVersion("2.0")
+from packaging.version import Version
+assert Version(tf.__version__) >= Version("2.0")
 
 tf.compat.v1.disable_eager_execution()
 
